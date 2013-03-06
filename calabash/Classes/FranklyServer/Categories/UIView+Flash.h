3ae9cc486a2bbb77e3bdd05091377177752bd3c9
Kra Larivain <olarivain@gmail.com>
2013-03-06 13:36:40 -0800

    - added flash category on UIView

---
+++ calabash/Classes/FranklyServer/Categories/UIView+Flash.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Flash.h
+//  calabash
+//
+//  Created by Olivier Larivain on 3/6/13.
+//  Copyright (c) 2013 LessPainful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Flash)
+
+- (void) FEX_flash;
+
+@end
