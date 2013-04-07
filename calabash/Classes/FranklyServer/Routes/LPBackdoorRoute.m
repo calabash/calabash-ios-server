dcc2d0ecad427af831c4aaedae8752360b9198e9
Karl Krukow <karl.krukow@gmail.com>
2013-04-07 12:18:16 +0200

    16#issuecomment-16011690

--- calabash/Classes/FranklyServer/Routes/LPBackdoorRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPBackdoorRoute.m
@@ -13,42 +13,44 @@
 - (BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path {
     return [method isEqualToString:@"POST"];
 }
-- (NSDictionary *)JSONResponseForMethod:(NSString *)method URI:(NSString *)path data:(NSDictionary*)data 
+- (NSDictionary *)JSONResponseForMethod:(NSString *)method URI:(NSString *)path data:(NSDictionary*)data
 {
-    NSString *selStr = [data objectForKey:@"selector"];
+    NSString *originalSelStr = [data objectForKey:@"selector"];
+    NSString *selStr = originalSelStr;
+    if (![originalSelStr hasSuffix:@":"]) {
+        selStr = [selStr stringByAppendingString:@":"];
+    }
+
     SEL sel = NSSelectorFromString(selStr);
-  
+    
     if ([[[UIApplication sharedApplication] delegate] respondsToSelector:sel]) {
         id arg = [data objectForKey:@"arg"];
-        NSString* res = [[[UIApplication sharedApplication] delegate] performSelector:sel withObject:arg];
-        if (!res) { res = @""; }
+        id res = [[[UIApplication sharedApplication] delegate] performSelector:sel withObject:arg];
+        if (!res) { res = [NSNull null]; }
         return [NSDictionary dictionaryWithObjectsAndKeys:
                 res , @"result",
                 @"SUCCESS",@"outcome",
                 nil];
     } else {
-      
-      if ([selStr hasSuffix:@":"] == NO) {
-        selStr = [selStr stringByAppendingString:@":"];
-      }
-      
+
       NSString *details = [NSString stringWithFormat:@"you must define the selector '%@' in your UIApplicationDelegate.",
                            selStr];
       NSString *exDecl0 = @"// declaration";
-      NSString *exDecl1 = [NSString stringWithFormat:@"- (NSString *) %@(NSString *) aArg;", selStr];
+      NSString *exDecl1 = [NSString stringWithFormat:@"-(id)%@ (id)arg;", selStr];
       NSString *exImp0 = @"// implementation";
-      NSString *exImp1 = [NSString stringWithFormat:@"- (NSString *) %@(NSString *) aArg {", selStr ];
-      NSString *exImp2 = [NSString stringWithFormat:@"  // examples"];
-      NSString *exImp3 = [NSString stringWithFormat:@"  return (2 + 2 == 5) ? @\"YES\" : @\"NO\";"];
-      NSString *exImp4 = [NSString stringWithFormat:@"  return \"{result:1,details:security pin set}\";"];
-      NSString *exImp5 = [NSString stringWithFormat:@"  return \"[briar,calabash,corncob,meerschaum]\";"];
-      NSString *exImp6 = [NSString stringWithFormat:@"}"];
-      NSString *usage0 = [NSString stringWithFormat:@"// usage"];
+      NSString *exImp1 = [NSString stringWithFormat:@"-(id)%@ (id)anArg {", selStr ];
+      NSString *exImp2 = [NSString stringWithFormat:@"  // do custom stuff"];
+      NSString *exImp3 = [NSString stringWithFormat:@"  // return examples"];
+      NSString *exImp4 = [NSString stringWithFormat:@"  return @\"OK\";"];
+      NSString *exImp5 = [NSString stringWithFormat:@"  return [NSArray arrayWithObjects:@\"a\",@\"b\",nil];"];
+      NSString *exImp6 = [NSString stringWithFormat:@"  return nil;"];
+      NSString *exImp7 = [NSString stringWithFormat:@"}"];
+      NSString *usage0 = [NSString stringWithFormat:@"// Ruby usage"];
       NSString *usage1 = [NSString stringWithFormat:@"backdoor('%@', '<arg>')", selStr];
-      
-      NSArray *exArr = [NSArray arrayWithObjects:details, @"\n", exDecl0, exDecl1, @"\n", exImp0, exImp1, exImp2, exImp3, exImp4, exImp5, exImp6, @"\n", usage0, usage1, nil];
+
+      NSArray *exArr = [NSArray arrayWithObjects:details, @"\n", exDecl0, exDecl1, @"\n", exImp0, exImp1, exImp2, exImp3, exImp4, exImp5, exImp6, exImp7, @"\n", usage0, usage1, nil];
       NSString *detailsStr = [exArr componentsJoinedByString:@"\n"];
-      
+
       NSString *reasonStr = [NSString stringWithFormat:@"application delegate does not respond to selector '%@'",
                              selStr];
         return [NSDictionary dictionaryWithObjectsAndKeys:
