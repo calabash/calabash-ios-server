aca6527ea48b370cac410c5562d9c30c0ab2bfa8
Karl Krukow <karl.krukow@gmail.com>
2013-05-20 18:24:52 +0200

    updated Frank and compat with NSDictionary

---
+++ calabash/Classes/FranklyServer/Categories/NSDictionary+Frank.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Frank.m
+//  calabash
+//
+//  Created by Karl Krukow on 5/20/13.
+//  Copyright (c) 2013 LessPainful. All rights reserved.
+//
+
+#import "NSDictionary+Frank.h"
+
+@implementation NSDictionary_Frank
+
+@end
