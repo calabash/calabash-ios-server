9fcd85c6bf3c27cc785a365afdb3c45d8c8ba2c5
Karl Krukow <karl.krukow@gmail.com>
2013-11-20 22:29:01 +0100

    bump version to 0.9.163.pre1

--- calabash/Classes/FranklyServer/Routes/LPVersionRoute.h
+++ calabash/Classes/FranklyServer/Routes/LPVersionRoute.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "LPRoute.h"
-#define kLPCALABASHVERSION @"0.9.162"
+#define kLPCALABASHVERSION @"0.9.163.pre1"
 @interface LPVersionRoute : NSObject<LPRoute>
 
 @end
