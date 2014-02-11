2a4b53e15d6898ec850d288e148bfe0fa32461ff
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:45:35 +0100

    reformatted LPUIARoute.m

--- calabash/Classes/FranklyServer/Routes/LPUIARoute.m
+++ calabash/Classes/FranklyServer/Routes/LPUIARoute.m
@@ -11,27 +11,22 @@
 
 @implementation LPUIARoute
 
-- (BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path {
-    return [method isEqualToString:@"POST"];
+- (BOOL) supportsMethod:(NSString *) method atPath:(NSString *) path {
+  return [method isEqualToString:@"POST"];
 }
 
--(void) beginOperation {
-    self.done = NO;
-    
-    NSString *command = [self.data objectForKey:@"command"];
-    [LPUIAChannel runAutomationCommand: command
-                                  then:  ^(NSDictionary *result)
-    {
-            if (!result) {
-                [self failWithMessageFormat:@"Timed out running command %@" message:command];
-            }
-            else {
-                [self succeedWithResult:[NSArray arrayWithObject:[[result copy] autorelease]]];
-            }
-    }];
 
-    
-    
+- (void) beginOperation {
+  self.done = NO;
+
+  NSString *command = [self.data objectForKey:@"command"];
+  [LPUIAChannel runAutomationCommand:command then:^(NSDictionary *result) {
+    if (!result) {
+      [self failWithMessageFormat:@"Timed out running command %@" message:command];
+    } else {
+      [self succeedWithResult:[NSArray arrayWithObject:[[result copy] autorelease]]];
+    }
+  }];
 }
 
 @end
