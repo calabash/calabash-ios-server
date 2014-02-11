dede3c68f1578f5b4c8e1f3e673a24fff419b8af
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:14:52 +0100

    reformatted LPInterploateRoute

--- calabash/Classes/FranklyServer/Routes/LPInterpolateRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPInterpolateRoute.m
@@ -7,153 +7,129 @@
 //
 
 #import "LPAsyncPlaybackRoute.h"
-#import "LPHTTPResponse.h"
 #import "LPHTTPConnection.h"
 #import "LPResources.h"
 #import "LPRecorder.h"
 #import "UIScriptParser.h"
 #import "LPTouchUtils.h"
-#import "LPJSONUtils.h"
 #import "LPInterpolateRoute.h"
 
 
 @implementation LPInterpolateRoute
 
-@synthesize events=_events;
+@synthesize events = _events;
 @synthesize parser1;
 @synthesize parser2;
 
 
 // Should only return YES after the LPHTTPConnection has read all available data.
-- (BOOL)isDone {
-    return !self.events && [super isDone];
+- (BOOL) isDone {
+  return !self.events && [super isDone];
 }
 
 
--(void)beginOperation 
-{
-    self.done = NO;
-    NSString *base64Events = [self.data objectForKey:@"events"];
-    id queryStart = [self.data objectForKey:@"start"];
-    id queryEnd = [self.data objectForKey:@"end"];
+- (void) beginOperation {
+  self.done = NO;
+  NSString *base64Events = [self.data objectForKey:@"events"];
+  id queryStart = [self.data objectForKey:@"start"];
+  id queryEnd = [self.data objectForKey:@"end"];
 
-    NSDictionary *offset_start = [self.data valueForKey:@"offset_start"];
-    NSNumber *off_start_x = [offset_start valueForKey:@"x"];
-    NSNumber *off_start_y = [offset_start valueForKey:@"y"];    
-        
-    CGPoint offsetPointStart = CGPointMake([off_start_x floatValue], [off_start_y floatValue]);
+  NSDictionary *offset_start = [self.data valueForKey:@"offset_start"];
+  NSNumber *off_start_x = [offset_start valueForKey:@"x"];
+  NSNumber *off_start_y = [offset_start valueForKey:@"y"];
 
-    NSDictionary *offset_end = [self.data valueForKey:@"offset_end"];
-    NSNumber *off_end_x = [offset_end valueForKey:@"x"];
-    NSNumber *off_end_y = [offset_end valueForKey:@"y"];    
-    
-    CGPoint offsetPointEnd = CGPointMake([off_end_x floatValue], [off_end_y floatValue]);
+  CGPoint offsetPointStart = CGPointMake([off_start_x floatValue], [off_start_y floatValue]);
 
-    UIView *targetView = nil;
-    self.parser1 = [UIScriptParser scriptParserWithObject: queryStart];
-    self.parser2 = [UIScriptParser scriptParserWithObject: queryEnd];
-    [self.parser1 parse];
-    [self.parser2 parse];
-    NSMutableArray* views = [NSMutableArray arrayWithCapacity:32];
-    for (UIWindow *window in [LPTouchUtils applicationWindows])
-    {
-        [views addObjectsFromArray:[window subviews]];
-    }
-    NSArray* resultStart = [self.parser1 evalWith:views];
-    NSArray* resultEnd = [self.parser2 evalWith:views];
+  NSDictionary *offset_end = [self.data valueForKey:@"offset_end"];
+  NSNumber *off_end_x = [offset_end valueForKey:@"x"];
+  NSNumber *off_end_y = [offset_end valueForKey:@"y"];
 
-    CGPoint centerStart;
-    if (resultStart == nil || [resultStart count] == 0)
-    {
-        centerStart = CGPointMake(0,0);
-    }
-    else
-    {
-        id startAt = [resultStart objectAtIndex:0];
-        if ([startAt isKindOfClass:[UIView class]])
-        {
-            centerStart = [LPTouchUtils centerOfView:startAt];
-        }
-        else
-        {
-            CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)[startAt valueForKey:@"center"], &centerStart);
-        }
+  CGPoint offsetPointEnd = CGPointMake([off_end_x floatValue], [off_end_y floatValue]);
 
-    }
-    centerStart = CGPointMake(centerStart.x + offsetPointStart.x, centerStart.y + offsetPointStart.y);
-            
-        
-    CGPoint centerEnd;
-    if (resultEnd == nil || [resultEnd count] == 0)
-    {
-        centerEnd = CGPointMake(0,0);
-    }
-    else {
-        id endAt = [resultEnd objectAtIndex:0];
-        targetView = endAt;
-        if ([endAt isKindOfClass:[UIView class]])
-        {
-            centerEnd = [LPTouchUtils centerOfView:endAt];
-        }
-        else
-        {
-            CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)[endAt valueForKey:@"center"], &centerEnd);
-        }
-    }
+  UIView *targetView = nil;
+  self.parser1 = [UIScriptParser scriptParserWithObject:queryStart];
+  self.parser2 = [UIScriptParser scriptParserWithObject:queryEnd];
+  [self.parser1 parse];
+  [self.parser2 parse];
+  NSMutableArray *views = [NSMutableArray arrayWithCapacity:32];
+  for (UIWindow *window in [LPTouchUtils applicationWindows]) {
+    [views addObjectsFromArray:[window subviews]];
+  }
+  NSArray *resultStart = [self.parser1 evalWith:views];
+  NSArray *resultEnd = [self.parser2 evalWith:views];
 
-    centerEnd = CGPointMake(centerEnd.x + offsetPointEnd.x, centerEnd.y + offsetPointEnd.y);
-
-
-    NSArray* baseEvents = [LPResources eventsFromEncoding:base64Events];
-    
-    
-    self.events = [LPResources interpolateEvents:baseEvents 
-                                       fromPoint:centerStart
-                                         toPoint:centerEnd];
-    
-    //NSLog(@"PLAY Events:\n%@",self.events);    
-    
-    [self play:self.events];
-    NSArray *resultArray = nil;
-    if (targetView == nil) {
-        resultArray = [NSArray array];
+  CGPoint centerStart;
+  if (resultStart == nil || [resultStart count] == 0) {
+    centerStart = CGPointMake(0, 0);
+  } else {
+    id startAt = [resultStart objectAtIndex:0];
+    if ([startAt isKindOfClass:[UIView class]]) {
+      centerStart = [LPTouchUtils centerOfView:startAt];
     } else {
-        resultArray = [NSArray arrayWithObject:targetView];
+      CGPointMakeWithDictionaryRepresentation((CFDictionaryRef) [startAt valueForKey:@"center"], &centerStart);
     }
-    
-    self.jsonResponse = [NSDictionary dictionaryWithObjectsAndKeys:
-                         resultArray, @"results",
-                         @"SUCCESS",@"outcome",
-                         nil];
-    
-    
+
+  }
+  centerStart = CGPointMake(centerStart.x + offsetPointStart.x, centerStart.y + offsetPointStart.y);
+
+
+  CGPoint centerEnd;
+  if (resultEnd == nil || [resultEnd count] == 0) {
+    centerEnd = CGPointMake(0, 0);
+  } else {
+    id endAt = [resultEnd objectAtIndex:0];
+    targetView = endAt;
+    if ([endAt isKindOfClass:[UIView class]]) {
+      centerEnd = [LPTouchUtils centerOfView:endAt];
+    } else {
+      CGPointMakeWithDictionaryRepresentation((CFDictionaryRef) [endAt valueForKey:@"center"], &centerEnd);
+    }
+  }
+
+  centerEnd = CGPointMake(centerEnd.x + offsetPointEnd.x, centerEnd.y + offsetPointEnd.y);
+
+
+  NSArray *baseEvents = [LPResources eventsFromEncoding:base64Events];
+
+
+  self.events = [LPResources interpolateEvents:baseEvents fromPoint:centerStart toPoint:centerEnd];
+
+  //NSLog(@"PLAY Events:\n%@",self.events);
+
+  [self play:self.events];
+  NSArray *resultArray = nil;
+  if (targetView == nil) {
+    resultArray = [NSArray array];
+  } else {
+    resultArray = [NSArray arrayWithObject:targetView];
+  }
+
+  self.jsonResponse = [NSDictionary dictionaryWithObjectsAndKeys:resultArray, @"results", @"SUCCESS", @"outcome", nil];
+
 }
 
 
-
-
-- (void) play:(NSArray *)events 
-{
-    [[LPRecorder sharedRecorder] load: self.events];
-    [[LPRecorder sharedRecorder] playbackWithDelegate: self doneSelector: @selector(playbackDone:)];    
+- (void) play:(NSArray *) events {
+  [[LPRecorder sharedRecorder] load:self.events];
+  [[LPRecorder sharedRecorder] playbackWithDelegate:self doneSelector:@selector(playbackDone:)];
 }
+
 
 //When this event occurs, jsonResponse has already been determined.
--(void) playbackDone:(NSDictionary *)details 
-{
-    self.done = YES;
-    self.events = nil;
-    self.parser1 = nil;
-    self.parser2 = nil;
-    [self.conn responseHasAvailableData:self];
+- (void) playbackDone:(NSDictionary *) details {
+  self.done = YES;
+  self.events = nil;
+  self.parser1 = nil;
+  self.parser2 = nil;
+  [self.conn responseHasAvailableData:self];
 }
 
--(void) dealloc 
-{
-    self.parser1 = nil;
-    self.parser2 = nil;
-    self.events = nil;
-    [super dealloc];
+
+- (void) dealloc {
+  self.parser1 = nil;
+  self.parser2 = nil;
+  self.events = nil;
+  [super dealloc];
 }
 
 @end
