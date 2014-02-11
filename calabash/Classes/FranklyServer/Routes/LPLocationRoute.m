130d87459e1e667597a98aa8f8190bd236362fe5
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:19:07 +0100

    BUG reformatted LPLocationRoute
    missing import of UIAutomation.h in LPLocationRoute.m

--- calabash/Classes/FranklyServer/Routes/LPLocationRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPLocationRoute.m
@@ -9,75 +9,46 @@
 
 #import "LPLocationRoute.h"
 
+// todo missing import of UIAutomation.h in LPLocationRoute.m
 @implementation LPLocationRoute
 
-- (BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path {
-    return [method isEqualToString:@"POST"];
+- (BOOL) supportsMethod:(NSString *) method atPath:(NSString *) path {
+  return [method isEqualToString:@"POST"];
 }
 
--(NSDictionary *)JSONResponseForMethod:(NSString *)method URI:(NSString *)path data:(NSDictionary*)data {
-    NSString *action = [data objectForKey:@"action"];
-    if ([action isEqualToString:@"change_location"])
-    {
-        NSNumber *lat = [data objectForKey:@"latitude"];
-        if (!lat)
-        {
-            return [NSDictionary dictionaryWithObjectsAndKeys:
-                                                 @"FAILURE",@"outcome",
-                                                 @"latitude must be specified",@"reason",
-                                                 @"",@"details",
-                                                nil];
-        }
-        NSNumber *lon = [data objectForKey:@"longitude"];
-        if (!lon)
-        {
-            return [NSDictionary dictionaryWithObjectsAndKeys:
-                                                 @"FAILURE",@"outcome",
-                                                 @"longitude must be specified",@"reason",
-                                                 @"",@"details",
-                                                 nil];
-        }
+
+- (NSDictionary *) JSONResponseForMethod:(NSString *) method URI:(NSString *) path data:(NSDictionary *) data {
+  NSString *action = [data objectForKey:@"action"];
+  if ([action isEqualToString:@"change_location"]) {
+    NSNumber *lat = [data objectForKey:@"latitude"];
+    if (!lat) {
+      return [NSDictionary dictionaryWithObjectsAndKeys:@"FAILURE", @"outcome", @"latitude must be specified", @"reason", @"", @"details", nil];
+    }
+    NSNumber *lon = [data objectForKey:@"longitude"];
+    if (!lon) {
+      return [NSDictionary dictionaryWithObjectsAndKeys:@"FAILURE", @"outcome", @"longitude must be specified", @"reason", @"", @"details", nil];
+    }
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-method-access"
 #pragma clang diagnostic ignored "-Wundeclared-selector"
-        id tgt = [NSClassFromString(@"UIATarget") localTarget];
-        if (tgt && [tgt respondsToSelector:@selector(setLocation:)])
-        {
-            [tgt setLocation:[NSDictionary dictionaryWithObjectsAndKeys:
-                               lat,@"latitude",
-                               lon,@"longitude",
-                              nil]];
-            return [NSDictionary dictionaryWithObjectsAndKeys:
-                                    [NSArray array], @"results",
-                                    @"SUCCESS",@"outcome",
-                                nil];
-        }
-        else
-        {
-            NSString *message = nil;
-            if (!tgt)
-            {
-                message = @"UIAutomation is not linked for some reason.";
-            }
-            else
-            {
-                message = @"setLocation is unsupported in this iOS version.";
-                                }
-                return [NSDictionary dictionaryWithObjectsAndKeys:
-                                                     @"FAILURE",@"outcome",
-                                                     message,@"reason",
-                                                     @"",@"details",
-                                                     nil];
-            }
+    id tgt = [NSClassFromString(@"UIATarget") localTarget];
+    if (tgt && [tgt respondsToSelector:@selector(setLocation:)]) {
+      [tgt setLocation:[NSDictionary dictionaryWithObjectsAndKeys:lat, @"latitude", lon, @"longitude", nil]];
+      return [NSDictionary dictionaryWithObjectsAndKeys:[NSArray array], @"results", @"SUCCESS", @"outcome", nil];
+    } else {
+      NSString *message = nil;
+      if (!tgt) {
+        message = @"UIAutomation is not linked for some reason.";
+      } else {
+        message = @"setLocation is unsupported in this iOS version.";
+      }
+      return [NSDictionary dictionaryWithObjectsAndKeys:@"FAILURE", @"outcome", message, @"reason", @"", @"details", nil];
     }
-    
+  }
+
 #pragma clang diagnostic pop
 
-    return [NSDictionary dictionaryWithObjectsAndKeys:
-                         @"FAILURE",@"outcome",
-                         [NSString stringWithFormat:@"action %@ not recognized",action],@"reason",
-                         @"",@"details",
-                         nil];
+  return [NSDictionary dictionaryWithObjectsAndKeys:@"FAILURE", @"outcome", [NSString stringWithFormat:@"action %@ not recognized", action], @"reason", @"", @"details", nil];
 }
 
 @end
