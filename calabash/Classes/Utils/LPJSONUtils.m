bd1ca02b852ea24589b3917918269252a29a20ab
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 02:20:57 +0100

    HAS BUG reformatted LPJSONUtils.h/m - found a bug and marked with todo
    check for accessibilityIdentifier is called on 'result' instead of
    'object'

--- calabash/Classes/Utils/LPJSONUtils.m
+++ calabash/Classes/Utils/LPJSONUtils.m
@@ -11,151 +11,120 @@
 
 @implementation LPJSONUtils
 
-+ (NSString*) serializeDictionary:(NSDictionary*) dictionary {
-    LPCJSONSerializer* s = [LPCJSONSerializer serializer];
-    NSError* error = nil;
-    NSData* d = [s serializeDictionary:dictionary error:&error];
-    if (error) {
-        NSLog(@"Unable to serialize dictionary (%@), %@",error,dictionary);
-    }
-    NSString* res = [[NSString alloc]  initWithBytes:[d bytes]
-                              length:[d length] encoding: NSUTF8StringEncoding];
-    return [res autorelease];
-}
-+ (NSDictionary*) deserializeDictionary:(NSString*) string {
-    LPCJSONDeserializer* ds = [LPCJSONDeserializer deserializer];
-    NSError* error = nil;
-    NSDictionary* res = [ds deserializeAsDictionary:[string dataUsingEncoding:NSUTF8StringEncoding]error:&error];
-    if (error) {
-        NSLog(@"Unable to deserialize  %@",string);
-    }
-    return res;
++ (NSString *) serializeDictionary:(NSDictionary *) dictionary {
+  LPCJSONSerializer *s = [LPCJSONSerializer serializer];
+  NSError *error = nil;
+  NSData *d = [s serializeDictionary:dictionary error:&error];
+  if (error) {
+    NSLog(@"Unable to serialize dictionary (%@), %@", error, dictionary);
+  }
+  NSString *res = [[NSString alloc] initWithBytes:[d bytes] length:[d length] encoding:NSUTF8StringEncoding];
+  return [res autorelease];
 }
 
-+ (NSString*) serializeArray:(NSArray*) array {
-    LPCJSONSerializer* s = [LPCJSONSerializer serializer];
-    NSError* error = nil;
-    NSData* d = [s serializeArray:array error:&error];
-    if (error) {
-        NSLog(@"Unable to serialize arrayy (%@), %@",error,array);
-    }
-    NSString* res = [[NSString alloc]  initWithBytes:[d bytes]
-                                              length:[d length] encoding: NSUTF8StringEncoding];
-    return [res autorelease];
-}
-+ (NSArray*) deserializeArray:(NSString*) string {
-    LPCJSONDeserializer* ds = [LPCJSONDeserializer deserializer];
-    NSError* error = nil;
-    NSArray* res = [ds deserializeAsArray:[string dataUsingEncoding:NSUTF8StringEncoding] error:&error];
-    if (error) {
-        NSLog(@"Unable to deserialize  %@",string);
-    }
-    return res;
++ (NSDictionary *) deserializeDictionary:(NSString *) string {
+  LPCJSONDeserializer *ds = [LPCJSONDeserializer deserializer];
+  NSError *error = nil;
+  NSDictionary *res = [ds deserializeAsDictionary:[string dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+  if (error) {
+    NSLog(@"Unable to deserialize  %@", string);
+  }
+  return res;
 }
 
-+(NSString *)serializeObject:(id)obj
-{
-    LPCJSONSerializer* s = [LPCJSONSerializer serializer];
-    NSError* error = nil;
-    NSData* d = [s serializeObject:obj error:&error];
-    if (error) {
-        NSLog(@"Unable to serialize object (%@), %@",error,[obj description]);
-    }
-    NSString* res = [[NSString alloc]  initWithBytes:[d bytes]
-                                              length:[d length] encoding: NSUTF8StringEncoding];
-    return [res autorelease];
++ (NSString *) serializeArray:(NSArray *) array {
+  LPCJSONSerializer *s = [LPCJSONSerializer serializer];
+  NSError *error = nil;
+  NSData *d = [s serializeArray:array error:&error];
+  if (error) {
+    NSLog(@"Unable to serialize arrayy (%@), %@", error, array);
+  }
+  NSString *res = [[NSString alloc] initWithBytes:[d bytes] length:[d length] encoding:NSUTF8StringEncoding];
+  return [res autorelease];
+}
+
++ (NSArray *) deserializeArray:(NSString *) string {
+  LPCJSONDeserializer *ds = [LPCJSONDeserializer deserializer];
+  NSError *error = nil;
+  NSArray *res = [ds deserializeAsArray:[string dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+  if (error) {
+    NSLog(@"Unable to deserialize  %@", string);
+  }
+  return res;
+}
+
++ (NSString *) serializeObject:(id) obj {
+  LPCJSONSerializer *s = [LPCJSONSerializer serializer];
+  NSError *error = nil;
+  NSData *d = [s serializeObject:obj error:&error];
+  if (error) {
+    NSLog(@"Unable to serialize object (%@), %@", error, [obj description]);
+  }
+  NSString *res = [[NSString alloc] initWithBytes:[d bytes] length:[d length] encoding:NSUTF8StringEncoding];
+  return [res autorelease];
 
 }
 
-+(id)jsonifyObject:(id)object
-{
-        if (!object) {return nil;}
-        if ([object isKindOfClass:[UIColor class]]) 
-        {
-            //todo special handling
-            return [object description];        
-        }
-        if ([object isKindOfClass:[UIView class]])
-        {
-            UIView *v = (UIView*)object;
-            NSMutableDictionary *result = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                           NSStringFromClass([object class]),@"class",
-                                           
-                                           nil];
-            
++ (id) jsonifyObject:(id) object {
+  if (!object) {return nil;}
+  if ([object isKindOfClass:[UIColor class]]) {
+    //todo special handling
+    return [object description];
+  }
+  if ([object isKindOfClass:[UIView class]]) {
+    UIView *v = (UIView *) object;
+    NSMutableDictionary *result = [NSMutableDictionary dictionaryWithObjectsAndKeys:NSStringFromClass([object class]), @"class",  nil];
 
-            NSString *lbl = [object accessibilityLabel];
-            if (lbl)
-            {
-                [result setObject:lbl forKey:@"label"];
-            }
-            else
-            {
-                [result setObject:[NSNull null] forKey:@"label"];                
-            }
 
-            if ([result respondsToSelector:@selector(accessibilityIdentifier)]) {
+    NSString *lbl = [object accessibilityLabel];
+    if (lbl) {
+      [result setObject:lbl forKey:@"label"];
+    } else {
+      [result setObject:[NSNull null] forKey:@"label"];
+    }
 
-                NSString *aid = [object accessibilityIdentifier];
-                if (aid)
-                {
-                    [result setObject:aid forKey:@"id"];
-                }
-                else
-                {
-                    [result setObject:[NSNull null] forKey:@"id"];
-                }
+    // TODO LPJSONUtils.h has bug around accessibilityIdentifier
+    if ([result respondsToSelector:@selector(accessibilityIdentifier)]) {
 
-            }
-            
-            
-            CGRect frame = [object frame];
-            
-            UIWindow *frontWindow = [[UIApplication sharedApplication] keyWindow];
-            UIWindow *window = [LPTouchUtils windowForView:v];
-            if (window)
-            {
-                CGRect rect = [window convertRect:v.bounds fromView:v];
-                rect = [frontWindow convertRect:rect fromWindow:window];
-                CGPoint center = [LPTouchUtils centerOfFrame:rect shouldTranslate:YES];
-                NSDictionary *rectDic =
-                [NSDictionary dictionaryWithObjectsAndKeys:
-                 [NSNumber numberWithFloat:center.x], @"center_x",
-                 [NSNumber numberWithFloat:center.y], @"center_y",
-                 [NSNumber numberWithFloat:rect.origin.x],@"x",
-                 [NSNumber numberWithFloat:rect.origin.y],@"y",
-                 [NSNumber numberWithFloat:rect.size.width],@"width",
-                 [NSNumber numberWithFloat:rect.size.height],@"height",
-                 nil];
-                
-                [result setObject:rectDic forKey:@"rect"];
-                
-                
-            }
-            
-            NSDictionary *frameDic =  
-            [NSDictionary dictionaryWithObjectsAndKeys:
-             [NSNumber numberWithFloat:frame.origin.x],@"x",
-             [NSNumber numberWithFloat:frame.origin.y],@"y",
-             [NSNumber numberWithFloat:frame.size.width],@"width",
-             [NSNumber numberWithFloat:frame.size.height],@"height",
-             nil];
-            
-            [result setObject:frameDic forKey:@"frame"];
-            
-            [result setObject:[object description] forKey:@"description"];
-            
-            return result;
-        }
-        
-        LPCJSONSerializer* s = [LPCJSONSerializer serializer];
-        NSError* error = nil;
-        if (![s serializeObject:object error:&error] || error) 
-        {
-            return [object description];
-        }    
-        return object;
-        
+      NSString *aid = [object accessibilityIdentifier];
+      if (aid) {
+        [result setObject:aid forKey:@"id"];
+      } else {
+        [result setObject:[NSNull null] forKey:@"id"];
+      }
 
+    }
+
+
+    CGRect frame = [object frame];
+
+    UIWindow *frontWindow = [[UIApplication sharedApplication] keyWindow];
+    UIWindow *window = [LPTouchUtils windowForView:v];
+    if (window) {
+      CGRect rect = [window convertRect:v.bounds fromView:v];
+      rect = [frontWindow convertRect:rect fromWindow:window];
+      CGPoint center = [LPTouchUtils centerOfFrame:rect shouldTranslate:YES];
+      NSDictionary *rectDic = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:center.x], @"center_x", [NSNumber numberWithFloat:center.y], @"center_y", [NSNumber numberWithFloat:rect.origin.x], @"x", [NSNumber numberWithFloat:rect.origin.y], @"y", [NSNumber numberWithFloat:rect.size.width], @"width", [NSNumber numberWithFloat:rect.size.height], @"height", nil];
+
+      [result setObject:rectDic forKey:@"rect"];
+
+
+    }
+
+    NSDictionary *frameDic = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:frame.origin.x], @"x", [NSNumber numberWithFloat:frame.origin.y], @"y", [NSNumber numberWithFloat:frame.size.width], @"width", [NSNumber numberWithFloat:frame.size.height], @"height", nil];
+
+    [result setObject:frameDic forKey:@"frame"];
+
+    [result setObject:[object description] forKey:@"description"];
+
+    return result;
+  }
+
+  LPCJSONSerializer *s = [LPCJSONSerializer serializer];
+  NSError *error = nil;
+  if (![s serializeObject:object error:&error] || error) {
+    return [object description];
+  }
+  return object;
 }
 @end
