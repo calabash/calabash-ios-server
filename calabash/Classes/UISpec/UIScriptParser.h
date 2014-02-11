fa35577c15f276ea5d4415f790197f7fefc674f8
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 02:50:11 +0100

    reformatted UIScriptParser

--- calabash/Classes/UISpec/UIScriptParser.h
+++ calabash/Classes/UISpec/UIScriptParser.h
@@ -7,21 +7,20 @@
 #import <Foundation/Foundation.h>
 
 @interface UIScriptParser : NSObject {
-    NSMutableArray *_res;
-    NSString *_script;
+  NSMutableArray *_res;
+  NSString *_script;
 }
 
-@property (nonatomic, retain) NSString* script;
-@property (nonatomic, retain) NSArray* arrayQuery;
+@property(nonatomic, retain) NSString *script;
+@property(nonatomic, retain) NSArray *arrayQuery;
 
-+(UIScriptParser*)scriptParserWithObject:(id)obj;
-+(UIView*)findViewByClass:(NSString*) className fromView:(UIView*) parent;
-- (id) initWithUIScript:(NSString*) script;
-- (id) initWithQuery:(NSArray*) aq;
 
++ (UIScriptParser *) scriptParserWithObject:(id) obj;
++ (UIView *) findViewByClass:(NSString *) className fromView:(UIView *) parent;
+- (id) initWithUIScript:(NSString *) script;
+- (id) initWithQuery:(NSArray *) aq;
 - (void) parse;
-- (NSArray*) parsedTokens;
-
-- (NSArray*) evalWith:(NSArray*) views;
+- (NSArray *) parsedTokens;
+- (NSArray *) evalWith:(NSArray *) views;
 
 @end
