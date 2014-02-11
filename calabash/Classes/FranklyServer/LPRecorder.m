b479b9b75a7c3cac511d8bb0b58d947528abe57d
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:52:31 +0100

    ANALYSIS reformatted LPRecorder
    dealloc does not playbackDelegate but it retains it  - this is a
    singleton so it hardly matters.
    
    however, it is very uncommon for classes to retain their delegates...

--- calabash/Classes/FranklyServer/LPRecorder.m
+++ calabash/Classes/FranklyServer/LPRecorder.m
@@ -1,94 +1,105 @@
-#import <UIKit/UIKit.h>
-
 #import "LPRecorder.h"
 
 
 @interface UIApplication (Recording)
 
--(void)_addRecorder:(id)recorder;
--(void)_removeRecorder:(id)recorder;
--(void)_playbackEvents:(NSArray*)events atPlaybackRate:(float)playbackRate messageWhenDone:(id)target withSelector:(SEL)selector;
+- (void) _addRecorder:(id) recorder;
+
+
+- (void) _removeRecorder:(id) recorder;
+
+
+- (void) _playbackEvents:(NSArray *) events atPlaybackRate:(float) playbackRate messageWhenDone:(id) target withSelector:(SEL) selector;
 
 @end
 
 static LPRecorder *sharedRecorder = nil;
 
 @implementation LPRecorder
-@synthesize isRecording=_isRecording;
-+(LPRecorder *)sharedRecorder {
-	if (sharedRecorder == nil) {
-		sharedRecorder = [[super allocWithZone:NULL] init];
-	}
-	return sharedRecorder;
+@synthesize isRecording = _isRecording;
+
+
++ (LPRecorder *) sharedRecorder {
+  if (sharedRecorder == nil) {
+    sharedRecorder = [[super allocWithZone:NULL] init];
+  }
+  return sharedRecorder;
 }
 
--(id)init {
-	self = [super init];
 
-	eventList = [[NSMutableArray alloc] init];
+- (id) init {
+  self = [super init];
 
-	return self;
+  eventList = [[NSMutableArray alloc] init];
+
+  return self;
 }
 
--(void)dealloc {
-	[eventList release];
-	[super dealloc];
+// todo dealloc does not playbackDelegate but it retains it
+- (void) dealloc {
+  [eventList release];
+  [super dealloc];
 }
 
--(void)record {
-	[eventList removeAllObjects];
 
-	NSLog(@"Starting recording");
-    _isRecording = YES;
-	[[UIApplication sharedApplication] _addRecorder: self];
-}
--(NSArray*)events {
-    return eventList;
-}
--(void)saveToFile:(NSString*)path {
-	NSLog(@"Saving events to file: %@", path);
-    
-	if ([eventList writeToFile: path atomically: YES]) {
-        NSLog(@"succeeded");
-    }
+- (void) record {
+  [eventList removeAllObjects];
+
+  NSLog(@"Starting recording");
+  _isRecording = YES;
+  [[UIApplication sharedApplication] _addRecorder:self];
 }
 
--(void)stop {
-	NSLog(@"Stopping recording");
-    _isRecording = NO;
-	[[UIApplication sharedApplication] _removeRecorder: self];
+
+- (NSArray *) events {
+  return eventList;
 }
 
--(void)recordApplicationEvent:(NSDictionary*)event {
-	NSLog(@"Recorded event: %@", event);
 
-	[eventList addObject:event];
+- (void) saveToFile:(NSString *) path {
+  NSLog(@"Saving events to file: %@", path);
+
+  if ([eventList writeToFile:path atomically:YES]) {
+    NSLog(@"succeeded");
+  }
 }
 
--(void)load:(NSArray*)events {
 
-	[eventList setArray: events];
+- (void) stop {
+  NSLog(@"Stopping recording");
+  _isRecording = NO;
+  [[UIApplication sharedApplication] _removeRecorder:self];
 }
 
--(void)loadFromFile:(NSString*)path {
 
-	[eventList setArray: [NSMutableArray arrayWithContentsOfFile: path]];
+- (void) recordApplicationEvent:(NSDictionary *) event {
+  NSLog(@"Recorded event: %@", event);
+  [eventList addObject:event];
 }
 
--(void)playbackWithDelegate: (id)delegate doneSelector:(SEL)doneSelector {
 
-	playbackDelegate = [delegate retain];
-    
-	playbackDoneSelector = doneSelector;
-
-	[[UIApplication sharedApplication] _playbackEvents: eventList atPlaybackRate: 1.0f messageWhenDone: self withSelector: @selector(playbackDone:)];
+- (void) load:(NSArray *) events {
+  [eventList setArray:events];
 }
 
--(void)playbackDone:(NSDictionary *)details {
 
-	[playbackDelegate performSelector: playbackDoneSelector];
-    [playbackDelegate release];
-    playbackDelegate=nil;
+- (void) loadFromFile:(NSString *) path {
+  [eventList setArray:[NSMutableArray arrayWithContentsOfFile:path]];
+}
+
+
+- (void) playbackWithDelegate:(id) delegate doneSelector:(SEL) doneSelector {
+  playbackDelegate = [delegate retain];
+  playbackDoneSelector = doneSelector;
+
+  [[UIApplication sharedApplication] _playbackEvents:eventList atPlaybackRate:1.0f messageWhenDone:self withSelector:@selector(playbackDone:)];
+}
+
+
+- (void) playbackDone:(NSDictionary *) details {
+  [playbackDelegate performSelector:playbackDoneSelector];
+  [playbackDelegate release];
+  playbackDelegate = nil;
 }
 
 @end
