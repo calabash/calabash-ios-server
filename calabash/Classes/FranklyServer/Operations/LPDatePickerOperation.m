420ee17272b1f9834bd8966c7309fb30c6d119e1
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:57:24 +0100

    reformatted LPDatePickerOperation

--- calabash/Classes/FranklyServer/Operations/LPDatePickerOperation.m
+++ calabash/Classes/FranklyServer/Operations/LPDatePickerOperation.m
@@ -9,12 +9,10 @@
 #import "LPDatePickerOperation.h"
 
 
-
-
 @implementation LPDatePickerOperation
 
 - (NSString *) description {
-	return [NSString stringWithFormat:@"DatePicker: %@",_arguments];
+  return [NSString stringWithFormat:@"DatePicker: %@", _arguments];
 }
 
 /*
@@ -26,20 +24,20 @@
 
 //                        required =========> |     optional
 // _arguments ==> [target date str, format str, notify targets, animated]
-- (id) performWithTarget:(UIView*)_view error:(NSError **)error {
+- (id) performWithTarget:(UIView *) _view error:(NSError **) error {
   if ([_view isKindOfClass:[UIDatePicker class]] == NO) {
-    NSLog(@"Warning view: %@ should be a date picker",_view);
+    NSLog(@"Warning view: %@ should be a date picker", _view);
     return nil;
   }
-  
+
   UIDatePicker *picker = (UIDatePicker *) _view;
-  
+
   NSString *dateStr = _arguments[0];
   if (dateStr == nil || [dateStr length] == 0) {
     NSLog(@"Warning: date str: '%@' should be non-nil and non-empty", dateStr);
     return nil;
   }
-  
+
   NSUInteger argcount = [_arguments count];
 
   NSString *dateFormat = nil;
@@ -49,18 +47,18 @@
     NSLog(@"Warning: date format is required as the second argument");
     return nil;
   }
-  
-  
+
+
   BOOL notifyTargets = YES;
   if (argcount > 2) {
     notifyTargets = [_arguments[2] boolValue];
   }
-  
+
   BOOL animate = YES;
   if (argcount > 3) {
     animate = [_arguments[3] boolValue];
   }
-  
+
   NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
   [formatter setDateFormat:dateFormat];
   NSDate *date = [formatter dateFromString:dateStr];
@@ -68,21 +66,19 @@
     NSLog(@"Warning: could not create date from '%@' and format '%@'", dateStr, dateFormat);
     return nil;
   }
-  
+
   NSDate *minDate = picker.minimumDate;
   if (minDate != nil && [date compare:minDate] == NSOrderedAscending) {
-    NSLog(@"Warning: could not set the date to '%@' because is earlier than the minimum date '%@'",
-          date, [minDate descriptionWithLocale:[NSLocale autoupdatingCurrentLocale]]);
+    NSLog(@"Warning: could not set the date to '%@' because is earlier than the minimum date '%@'", date, [minDate descriptionWithLocale:[NSLocale autoupdatingCurrentLocale]]);
     return nil;
   }
-  
+
   NSDate *maxDate = picker.maximumDate;
   if (maxDate != nil && [date compare:maxDate] == NSOrderedDescending) {
-    NSLog(@"Warning: could not set the date to '%@' because is later than the maximum date '%@'",
-          date, [maxDate descriptionWithLocale:[NSLocale autoupdatingCurrentLocale]]);
+    NSLog(@"Warning: could not set the date to '%@' because is later than the maximum date '%@'", date, [maxDate descriptionWithLocale:[NSLocale autoupdatingCurrentLocale]]);
     return nil;
   }
-  
+
   [picker setDate:date animated:animate];
 
   if (notifyTargets) {
