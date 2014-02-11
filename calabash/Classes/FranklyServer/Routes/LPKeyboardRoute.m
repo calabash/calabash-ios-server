e70852ba55f566e52d23284c8096c9ad0b8348dd
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 03:16:32 +0100

    BUG reformatted LPKeyboardRoute
    missing dealloc method and _events ivar is not released

--- calabash/Classes/FranklyServer/Routes/LPKeyboardRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPKeyboardRoute.m
@@ -7,118 +7,101 @@
 //
 
 #import "LPKeyboardRoute.h"
-#import "LPHTTPResponse.h"
 #import "LPHTTPConnection.h"
 #import "LPResources.h"
 #import "LPRecorder.h"
 #import "UIScriptParser.h"
 #import "LPTouchUtils.h"
-#import "LPJSONUtils.h"
-#import <QuartzCore/QuartzCore.h>
 
+// todo missing dealloc method and _events ivar is not released
 @implementation LPKeyboardRoute
 
--(void)beginOperation
-{
-    _events = nil;
-    _playbackDone = NO;    
-    NSString *characterString = [self.data objectForKey:@"key"];
-    
-    
-    NSArray *events = [LPResources eventsFromEncoding:[self.data objectForKey:@"events"]]; 
-    UIWindow *keyboardWindow = nil;
+- (void) beginOperation {
+  _events = nil;
+  _playbackDone = NO;
+  NSString *characterString = [self.data objectForKey:@"key"];
 
-    for (UIWindow *window in [LPTouchUtils applicationWindows])
-    {
-        if ([NSStringFromClass([window class]) isEqual:@"UITextEffectsWindow"]) 
-        {
-            keyboardWindow = window;
-            break;
-        }
+
+  NSArray *events = [LPResources eventsFromEncoding:[self.data objectForKey:@"events"]];
+  UIWindow *keyboardWindow = nil;
+
+  for (UIWindow *window in [LPTouchUtils applicationWindows]) {
+    if ([NSStringFromClass([window class]) isEqual:@"UITextEffectsWindow"]) {
+      keyboardWindow = window;
+      break;
     }
-    
-    if (!keyboardWindow) 
-    {
-        _playbackDone = YES;
-        return [self failWithMessageFormat:@"No keyboard displaying..." message:nil];
+  }
+
+  if (!keyboardWindow) {
+    _playbackDone = YES;
+    return [self failWithMessageFormat:@"No keyboard displaying..." message:nil];
+  }
+
+
+  UIView *keyboardView = [UIScriptParser findViewByClass:@"UIKBKeyplaneView" fromView:keyboardWindow];
+
+
+  if (!keyboardView) {
+    _playbackDone = YES;
+    return [self failWithMessageFormat:@"Found not UIKBKeyplaneView..." message:nil];
+  }
+
+
+
+  //cf KIF: https://github.com/square/KIF/blob/master/Classes/KIFTestStep.m
+
+  // If we didn't find the standard keyboard view, then we may have a custom keyboard
+
+  id /*UIKBKeyplane*/ keyplane = [keyboardView valueForKey:@"keyplane"];
+  NSArray *keys = [keyplane valueForKey:@"keys"];
+
+  id keyToTap = nil;
+
+  for (id/*UIKBKey*/ key in keys) {
+    NSString *representedString = [key valueForKey:@"representedString"];
+    // Find the key based on the key's represented string
+    if ([representedString isEqual:characterString]) {
+      keyToTap = key;
     }
 
-    
+    if ([representedString length] > 0) {
 
-    UIView *keyboardView = [UIScriptParser findViewByClass:@"UIKBKeyplaneView" fromView:keyboardWindow];
-    
-    
-    if (!keyboardView)
-    {
-        _playbackDone = YES;
-        return [self failWithMessageFormat:@"Found not UIKBKeyplaneView..." message:nil];        
+      if ([characterString isEqualToString:@"Return"] && [representedString characterAtIndex:0] == (unichar) 10) {
+        keyToTap = key;
+      }
     }
+  }
 
-    
-    
-    //cf KIF: https://github.com/square/KIF/blob/master/Classes/KIFTestStep.m
-    
-    // If we didn't find the standard keyboard view, then we may have a custom keyboard
-    
-    id /*UIKBKeyplane*/ keyplane = [keyboardView valueForKey:@"keyplane"];
-    NSArray *keys = [keyplane valueForKey:@"keys"];
-    
-    id keyToTap = nil;
-    
-    for (id/*UIKBKey*/ key in keys) {
-        NSString *representedString = [key valueForKey:@"representedString"];
-        // Find the key based on the key's represented string
-        if ([representedString isEqual:characterString]) 
-        {
-            keyToTap = key;
-        }
-        
-        if ([representedString length]>0)
-        {
-            
-            if ([characterString isEqualToString:@"Return"] && [representedString characterAtIndex:0]==(unichar)10)
-            {
-                keyToTap = key;
-            }
-        }
-    }             
-    
-    if (keyToTap) 
-    {
-        UIView *v = [UIScriptParser findViewByClass:@"UIKeyboardAutomatic" fromView:keyboardWindow];
-        UIView *sup = [v superview];
-        
-        CGRect parentFrame = [sup convertRect:v.frame toView:nil];//[sup convertRect:v.frame toView:sup];
-        
-        CGRect frame = [keyToTap frame];
-        CGPoint point = CGPointMake(parentFrame.origin.x + frame.origin.x + 0.5 * frame.size.width,
-                                    parentFrame.origin.y + frame.origin.y + 0.5 * frame.size.height);
-        
-        point = [(UIWindow*)keyboardWindow convertPoint:point toWindow:nil];
-        point=[LPTouchUtils translateToScreenCoords:point];
-        _events =  [[LPResources transformEvents:events toPoint:point] retain];
-        self.done = YES;
-        self.jsonResponse = [NSDictionary dictionaryWithObjectsAndKeys:
-                             [NSArray arrayWithObject:v], @"results",
-                             @"SUCCESS",@"outcome",
-                             nil];
+  if (keyToTap) {
+    UIView *v = [UIScriptParser findViewByClass:@"UIKeyboardAutomatic" fromView:keyboardWindow];
+    UIView *sup = [v superview];
 
-        [self play:_events];
-        
-    }
-    else 
-    {
-        _playbackDone = YES;
-        [self failWithMessageFormat:@"Found no key: %@" message:characterString];        
-    }
+    CGRect parentFrame = [sup convertRect:v.frame toView:nil];//[sup convertRect:v.frame toView:sup];
+
+    CGRect frame = [keyToTap frame];
+    CGPoint point = CGPointMake(parentFrame.origin.x + frame.origin.x + 0.5 * frame.size.width, parentFrame.origin.y + frame.origin.y + 0.5 * frame.size.height);
+
+    point = [(UIWindow *) keyboardWindow convertPoint:point toWindow:nil];
+    point = [LPTouchUtils translateToScreenCoords:point];
+    _events = [[LPResources transformEvents:events toPoint:point] retain];
+    self.done = YES;
+    self.jsonResponse = [NSDictionary dictionaryWithObjectsAndKeys:[NSArray arrayWithObject:v], @"results", @"SUCCESS", @"outcome", nil];
+
+    [self play:_events];
+
+  } else {
+    _playbackDone = YES;
+    [self failWithMessageFormat:@"Found no key: %@" message:characterString];
+  }
 
 }
 
--(void) play:(NSArray *)events {
-    _playbackDone = NO;
-    [[LPRecorder sharedRecorder] load: _events];
-    [[LPRecorder sharedRecorder] playbackWithDelegate: self doneSelector: @selector(playbackDone:)];
-    
+
+- (void) play:(NSArray *) events {
+  _playbackDone = NO;
+  [[LPRecorder sharedRecorder] load:_events];
+  [[LPRecorder sharedRecorder] playbackWithDelegate:self doneSelector:@selector(playbackDone:)];
+
 }
 
 //-(void) waitUntilPlaybackDone {
@@ -127,20 +110,19 @@
 //    }
 //}
 
--(void) playbackDone:(NSDictionary *)details {
-    _playbackDone = YES;
-    [_events release];
-    _events=nil;
-    [self.conn responseHasAvailableData:self];
+- (void) playbackDone:(NSDictionary *) details {
+  _playbackDone = YES;
+  [_events release];
+  _events = nil;
+  [self.conn responseHasAvailableData:self];
 
 }
+
 
 // Should only return YES after the LPHTTPConnection has read all available data.
-- (BOOL)isDone 
-{
-    return _playbackDone && [super isDone];
+- (BOOL) isDone {
+  return _playbackDone && [super isDone];
 }
-
 
 
 @end
