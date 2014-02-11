18200fe35d67d1cb93f84714697f710b5a303ec4
Joshua Moody <joshuajmoody@gmail.com>
2014-02-11 02:46:05 +0100

    reformatted UIScriptASTPredicate

--- calabash/Classes/UISpec/UIScriptASTPredicate.m
+++ calabash/Classes/UISpec/UIScriptASTPredicate.m
@@ -7,45 +7,44 @@
 //
 
 #import "UIScriptASTPredicate.h"
-#import "LPTouchUtils.h"
 
 @implementation UIScriptASTPredicate
-@synthesize predicate=_predicate;
-@synthesize selector=_selector;
+@synthesize predicate = _predicate;
+@synthesize selector = _selector;
 
--(id) initWithPredicate:(NSPredicate *)pred selector:(SEL) sel {
-    self = [super init];
-    if (self) {
-        self.predicate = pred;
-        self.selector = sel;
+
+- (id) initWithPredicate:(NSPredicate *) pred selector:(SEL) sel {
+  self = [super init];
+  if (self) {
+    self.predicate = pred;
+    self.selector = sel;
+  }
+  return self;
+}
+
+
+- (NSString *) description {
+  return [NSString stringWithFormat:@"UIScriptASTPredicate: %@", [self.predicate description]];
+}
+
+
+- (NSMutableArray *) evalWith:(NSArray *) views direction:(UIScriptASTDirectionType) dir visibility:(UIScriptASTVisibilityType) visibility {
+
+  NSMutableArray *res = [NSMutableArray arrayWithCapacity:8];
+  for (id v in views) {
+    if ([v isKindOfClass:[NSDictionary class]] || [v respondsToSelector:self.selector]) {
+      if ([self.predicate evaluateWithObject:v]) {
+        [res addObject:v];
+      }
     }
-    return self;
+  }
+  return res;
 }
 
-- (NSString*) description {
-    return [NSString stringWithFormat:@"UIScriptASTPredicate: %@",[self.predicate description]];
-}
-
-- (NSMutableArray*) evalWith:(NSArray*) views
-                   direction:(UIScriptASTDirectionType) dir
-                  visibility:(UIScriptASTVisibilityType)visibility {
-
-    NSMutableArray* res = [NSMutableArray arrayWithCapacity:8];    
-    for (id v in views) {
-        if ([v isKindOfClass:[NSDictionary class]] || [v respondsToSelector:self.selector])
-        {
-            if([self.predicate evaluateWithObject:v]) {
-                [res addObject:v];
-            }
-        }
-            
-    }
-    return res;
-}
 
 - (void) dealloc {
-    self.predicate=nil;
-    [super dealloc];
+  self.predicate = nil;
+  [super dealloc];
 }
 
 @end
