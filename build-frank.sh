686c99dd42f4b5e7ca2da9afe14533160d28ea7c
Joshua Moody <joshuajmoody@gmail.com>
2014-07-30 00:49:49 +0200

    frank script should use xcrun and xcpretty

--- build-frank.sh
+++ build-frank.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-rm -fr build && xcodebuild -target "frank-calabash" -configuration Debug SYMROOT=build SDKROOT=iphonesimulator IPHONEOS_DEPLOYMENT_TARGET=5.1.1
+#!/usr/bin/env bash
+
+# todo add to the Makefile as 'make frank' or 'make' task
+
+rm -rf build
+
+# xcpretty will reduce spam and build times
+XCPRETTY=`gem list xcpretty -i | tr -d '\n'`
+if [ "${XCPRETTY}" = "false" ]; then gem install xcpretty; fi
+
+xcrun xcodebuild -target "frank-calabash" \
+                 -configuration Debug \
+                 SYMROOT=build \
+                 SDKROOT=iphonesimulator \
+                 IPHONEOS_DEPLOYMENT_TARGET=5.1.1 | xcpretty -c
