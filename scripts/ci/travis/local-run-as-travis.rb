9cb2d49b360bd1a9e9a8e0dac4355a23ed941b80
Joshua Moody <joshuajmoody@gmail.com>
2014-08-19 01:49:48 +0200

    unit tests have out lived their usefulness

--- scripts/ci/travis/local-run-as-travis.rb
+++ scripts/ci/travis/local-run-as-travis.rb
@@ -7,10 +7,6 @@ working_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..
 Dir.chdir working_dir do
   install_gem('xcpretty')
   do_system('make clean')
-  scripts_dir = File.expand_path(File.join(working_dir, 'scripts'))
-  Dir.chdir scripts_dir do
-    do_system 'ruby make-libraries.rb'
-  end
   do_system('make')
   do_system('make frank')
   do_system('scripts/ci/travis/run-chou-tests.rb')
