ae2fe4bf2dd18dc9e4eceef8ef951f89e9c93638
Joshua Moody <joshuajmoody@gmail.com>
2014-08-19 01:51:45 +0200

    ci should test all make rules

--- scripts/ci/travis/local-run-as-travis.rb
+++ scripts/ci/travis/local-run-as-travis.rb
@@ -7,8 +7,10 @@ working_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..
 Dir.chdir working_dir do
   install_gem('xcpretty')
   do_system('make clean')
-  do_system('make')
+  do_system('make framework')
   do_system('make frank')
+  do_system('make dylib')
+  do_system('make all')
   do_system('scripts/ci/travis/run-chou-tests.rb')
   do_system('test-make-without-xcpretty.rb')
 end
