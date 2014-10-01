c7557a3fbcb56f703db6f0cc5d1e031fdeaa58a4
n-studio <contact@n-studio.fr>
2014-10-01 13:45:23 +0900

    fix 4inch detection

--- calabash/Classes/Utils/LPTouchUtils.m
+++ calabash/Classes/Utils/LPTouchUtils.m
@@ -24,7 +24,7 @@
   BOOL iphone5Like = NO;
   if ([@"iPhone Simulator" isEqualToString:[device model]]) {
 
-    NSPredicate *inch5PhonePred = [NSPredicate predicateWithFormat:@"IPHONE_SIMULATOR_VERSIONS LIKE '*iPhone*Retina*4-inch*'"];
+    NSPredicate *inch5PhonePred = [NSPredicate predicateWithFormat:@"IPHONE_SIMULATOR_VERSIONS LIKE '*iPhone*Retina*4-inch*' OR SIMULATOR_VERSION_INFO LIKE '*iPhone 5*' OR SIMULATOR_VERSION_INFO LIKE '*iPhone 6*'"];
     iphone5Like = [inch5PhonePred evaluateWithObject:env];
   } else if ([[device model] hasPrefix:@"iPhone"]) {
     iphone5Like = [system hasPrefix:@"iPhone5"] || [system hasPrefix:@"iPhone6"];
