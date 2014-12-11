cad52a230a46ce7dc9cb0cfb3cdb9b2d07bb601c
Joshua Moody <joshuajmoody@gmail.com>
2014-12-11 17:01:08 +0100

    Fetch server port with new LPInfoPlist class
    An interface for interacting with the app InfoPlist

---
+++ calabash/Classes/Utils/LPInfoPlist.m
@@ -0,0 +1,22 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "LPInfoPlist.h"
+
+static unsigned short const LPCalabashServerDefaultPort = 37265;
+static NSString *const LPCalabashServerPortInfoPlistKey = @"CalabashServerPort";
+
+@implementation LPInfoPlist
+
+- (unsigned short) serverPort {
+  NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+  NSNumber *infoPlistValue = info[LPCalabashServerPortInfoPlistKey];
+  if (!infoPlistValue) {
+    return LPCalabashServerDefaultPort;
+  } else {
+    return [infoPlistValue unsignedShortValue];
+  }
+}
+
+@end
