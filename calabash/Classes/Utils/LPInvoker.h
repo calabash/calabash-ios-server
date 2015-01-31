8acb4b52a5111384ca928403b9768b1812c9aea6
Joshua Moody <joshuajmoody@gmail.com>
2015-01-31 03:06:01 +0100

    LPInvoker public interface

--- calabash/Classes/Utils/LPInvoker.h
+++ calabash/Classes/Utils/LPInvoker.h
@@ -2,4 +2,15 @@
 
 @interface LPInvoker : NSObject
 
+@property(assign, nonatomic, readonly) SEL selector;
+@property(strong, nonatomic, readonly) id receiver;
+
+- (id) initWithSelector:(SEL) selector
+               receiver:(id) receiver;
+
+- (BOOL) receiverRespondsToSelector;
+- (NSString *) encoding;
+- (id) invoke;
+- (BOOL) encodingIsUnhandled;
+
 @end
