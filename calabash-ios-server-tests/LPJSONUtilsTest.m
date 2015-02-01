c855012f99d29a4379d50c73b4efe6ab8a160a4d
Joshua Moody <joshuajmoody@gmail.com>
2015-02-01 01:32:56 +0100

    Sanity test for jsonifyView:

--- calabash-ios-server-tests/LPJSONUtilsTest.m
+++ calabash-ios-server-tests/LPJSONUtilsTest.m
@@ -7,6 +7,12 @@
 #import "LPJSONUtils.h"
 #import <OCMock/OCMock.h>
 
+@interface LPJSONUtils (LPXCTTEST)
+
++ (NSMutableDictionary *) jsonifyView:(UIView *) view;
+
+@end
+
 @interface LPJSONUtilsTest : XCTestCase
 
 @end
@@ -19,6 +25,29 @@
 
 - (void)tearDown {
   [super tearDown];
+}
+
+#pragma mark - jsonifyView
+
+- (void) testJsonifyView {
+  CGRect frame = {20, 64, 88, 44};
+  UIView *view = [[UIView alloc] initWithFrame:frame];
+  NSDictionary *dict = [LPJSONUtils jsonifyView:view];
+  NSLog(@"%@", dict);
+
+  XCTAssertEqualObjects(dict[@"accessibilityElement"], @(0));
+  XCTAssertEqualObjects(dict[@"alpha"], @(1));
+  XCTAssertEqualObjects(dict[@"class"], @"UIView");
+  XCTAssertEqualObjects(dict[@"description"], [view description]);
+  XCTAssertEqualObjects(dict[@"enabled"], @(1));
+  XCTAssertEqualObjects(dict[@"frame"][@"x"], @(20));
+  XCTAssertEqualObjects(dict[@"frame"][@"y"], @(64));
+  XCTAssertEqualObjects(dict[@"frame"][@"width"], @(88));
+  XCTAssertEqualObjects(dict[@"frame"][@"height"], @(44));
+  XCTAssertEqualObjects(dict[@"id"], [NSNull null]);
+  XCTAssertEqualObjects(dict[@"label"], [NSNull null]);
+  XCTAssertEqualObjects(dict[@"value"], [NSNull null]);
+  XCTAssertEqualObjects(dict[@"visible"], @(1));
 }
 
 @end
