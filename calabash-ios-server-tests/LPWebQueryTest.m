72280d9d361823668fe9c2b7264a8d58a7e1abd1
Joshua Moody <joshuajmoody@gmail.com>
2015-02-23 22:51:20 +0100

    Add LPWebQueryTest to XCTest bundle

---
+++ calabash-ios-server-tests/LPWebQueryTest.m
@@ -0,0 +1,26 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "LPWebQuery.h"
+#import <OCMock/OCMock.h>
+
+@interface LPWebQueryTest : XCTestCase
+
+
+@end
+
+@implementation LPWebQueryTest
+
+- (void) setUp {
+  [super setUp];
+}
+
+- (void) tearDown {
+  [super tearDown];
+}
+
+
+@end
