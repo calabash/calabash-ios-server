2e8c2f4e05e77cface28f7dcecd2db6f51acb2c3
Joshua Moody <joshuajmoody@gmail.com>
2015-03-05 13:55:34 +0100

    Unit tests screenshot route

--- calabash-ios-server-tests/LPScreenshotRouteTest.m
+++ calabash-ios-server-tests/LPScreenshotRouteTest.m
@@ -3,11 +3,54 @@
 #endif
 
 #import "LPScreenshotRoute.h"
+#import "LPHTTPResponse.h"
 
-@interface LPScreenshotRouteTest : XCTestCase
+@interface LPScreenshotRoute (LPTEST)
+
+- (NSObject <LPHTTPResponse> *) httpResponseForMethod:(NSString *) method
+                                                  URI:(NSString *) path;
 
 @end
 
-@implementation LPScreenshotRouteTest
+SpecBegin(LPScreenshotRoute)
 
-@end
+describe(@"LPScreenshotRoute", ^{
+
+  describe(@"#supportsMethod:atPath:", ^{
+    it(@"returns true for GET", ^{
+      LPScreenshotRoute *route = [LPScreenshotRoute new];
+      BOOL expected = [route supportsMethod:@"GET" atPath:nil];
+      expect(expected).to.equal(YES);
+    });
+
+    it(@"returns false otherwise", ^{
+      LPScreenshotRoute *route = [LPScreenshotRoute new];
+      BOOL expected = [route supportsMethod:@"BAR" atPath:nil];
+      expect(expected).to.equal(NO);
+    });
+  });
+
+  describe(@"#httpResponseForMethod:URI:", ^{
+    it(@"returns an object that conforms to LPHTTPResponse", ^{
+      LPScreenshotRoute *route = [LPScreenshotRoute new];
+      id mock = OCMPartialMock(route);
+      OCMExpect([mock takeScreenshot]).andReturn([NSData data]);
+
+      id result = [mock httpResponseForMethod:nil URI:nil];
+      XCTAssertNotNil(result);
+      expect(result).to.conformTo(@protocol(LPHTTPResponse));
+      OCMVerify([mock takeScreenshot]);
+    });
+  });
+
+  describe(@"#takeScreenshot", ^{
+    it(@"returns data", ^{
+      LPScreenshotRoute *route = [LPScreenshotRoute new];
+      NSData *result = [route takeScreenshot];
+      XCTAssertNotNil(result);
+      expect(result).to.beKindOf([NSData class]);
+    });
+  });
+});
+
+SpecEnd
