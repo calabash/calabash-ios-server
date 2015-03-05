d9af9d58c26e9611ffbce6e85d654a9f029300d1
Joshua Moody <joshuajmoody@gmail.com>
2015-03-05 12:07:16 +0100

    Add screenshot route tests

---
+++ calabash-ios-server-tests/LPScreenshotRouteTest.m
@@ -0,0 +1,13 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "LPScreenshotRoute.h"
+
+@interface LPScreenshotRouteTest : XCTestCase
+
+@end
+
+@implementation LPScreenshotRouteTest
+
+@end
