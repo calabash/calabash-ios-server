a9492d74ff33dee999b12713c57c478f0d98ff02
Joshua Moody <joshuajmoody@gmail.com>
2015-03-06 04:47:21 +0100

    LPDevice: put under ARC

--- calabash/Classes/Utils/LPDevice.m
+++ calabash/Classes/Utils/LPDevice.m
@@ -1,3 +1,6 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
 //
 //  LPDevice.m
 //  calabash
@@ -131,12 +134,6 @@
 - (NSDictionary*) screenDimensions {
   return _screenDimensions;
 }
-
-- (void) dealloc {
-  [_screenDimensions release];
-  [super dealloc];
-}
-
 
 
 @end
