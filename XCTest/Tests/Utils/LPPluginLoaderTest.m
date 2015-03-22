a6ab4e050ddbfe58e3267542833b472e02ada8ba
Joshua Moody <joshuajmoody@gmail.com>
2015-03-22 23:41:19 +0100

    Add pluging loader test

---
+++ XCTest/Tests/Utils/LPPluginLoaderTest.m
@@ -0,0 +1,11 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+@interface LPPluginLoaderTest : XCTestCase
+
+@end
+
+@implementation LPPluginLoaderTest
+
+@end
