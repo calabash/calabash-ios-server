133d3cfda08a6f1a1b8dcce54a1ed8bdaf324e79
Joshua Moody <joshuajmoody@gmail.com>
2015-04-07 22:35:35 +0200

    Add XCTest for setText operation

---
+++ XCTest/Tests/Operations/LPSetTextOperationTest.m
@@ -0,0 +1,12 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "LPSetTextOperation.h"
+
+SpecBegin(LPSetTextOperation)
+
+describe(@"LPSetTextOperation", ^{
+
+});
+SpecEnd
