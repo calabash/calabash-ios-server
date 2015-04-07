1eca66dc854e6ee5684e40aa7f81c52915bd013d
Joshua Moody <joshuajmoody@gmail.com>
2015-04-07 22:10:54 +0200

    Convert LPSetTextOperation to ARC

--- calabash/Classes/FranklyServer/Operations/LPSetTextOperation.m
+++ calabash/Classes/FranklyServer/Operations/LPSetTextOperation.m
@@ -1,3 +1,7 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
 //  Created by Karl Krukow on 11/09/11.
 //  Copyright (c) 2011 LessPainful. All rights reserved.
 
