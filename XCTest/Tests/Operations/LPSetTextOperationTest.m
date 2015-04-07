cbfeeef6ee829fded31769ff361cb4ee5d641e17
Joshua Moody <joshuajmoody@gmail.com>
2015-04-07 22:40:30 +0200

    Stub out SetTextOperation tests

--- XCTest/Tests/Operations/LPSetTextOperationTest.m
+++ XCTest/Tests/Operations/LPSetTextOperationTest.m
@@ -8,5 +8,25 @@ SpecBegin(LPSetTextOperation)
 
 describe(@"LPSetTextOperation", ^{
 
+  describe(@"performWithTarget:error:", ^{
+    describe(@"target represents a WebView; it is a dictionary", ^{
+
+      describe(@"dict has invalid keys", ^{
+
+      });
+
+      describe(@"dict has valid keys", ^{
+
+      });
+    });
+
+    describe(@"target responds to setText", ^{
+
+    });
+
+    it(@"target does not respond to setText", ^{
+
+    });
+  });
 });
 SpecEnd
