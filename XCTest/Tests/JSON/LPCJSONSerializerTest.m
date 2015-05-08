2921b71cdaf6c3c29f3a973d740dd28d7458c828
Joshua Moody <joshuajmoody@gmail.com>
2015-05-08 19:29:07 +0200

    Add LPCJSONSerializerTest

---
+++ XCTest/Tests/JSON/LPCJSONSerializerTest.m
@@ -0,0 +1,13 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "LPCJSONSerializer.h"
+
+SpecBegin(LPCJSONSerializer)
+
+describe(@"LPCJSONSerializer", ^{
+
+});
+
+SpecEnd
