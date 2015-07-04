a8f19bb5223d4dd8edb4ba953ba5201e59c9440f
Joshua Moody <joshuajmoody@gmail.com>
2015-07-04 10:51:08 +0200

    Update FileResponse

--- calabash/Classes/CocoaHttpServer/Responses/LPHTTPFileResponse.h
+++ calabash/Classes/CocoaHttpServer/Responses/LPHTTPFileResponse.h
@@ -4,22 +4,22 @@
 @class LPHTTPConnection;
 
 
-@interface LPHTTPFileResponse : NSObject <LPHTTPResponse>
-{
-	LPHTTPConnection *connection;
-	
-	NSString *filePath;
-	UInt64 fileLength;
-	UInt64 fileOffset;
-	
-	BOOL aborted;
-	
-	int fileFD;
-	void *buffer;
-	NSUInteger bufferSize;
+@interface LPHTTPFileResponse : NSObject <LPHTTPResponse> {
+  LPHTTPConnection *connection;
+
+  NSString *filePath;
+  UInt64 fileLength;
+  UInt64 fileOffset;
+
+  BOOL aborted;
+
+  int fileFD;
+  void *buffer;
+  NSUInteger bufferSize;
 }
 
 - (id)initWithFilePath:(NSString *)filePath forConnection:(LPHTTPConnection *)connection;
+
 - (NSString *)filePath;
 
 @end
