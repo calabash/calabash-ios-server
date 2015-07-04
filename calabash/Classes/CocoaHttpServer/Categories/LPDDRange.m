f6de7e9474d1f13dff8dc0f579322f7e9df8a9fe
Joshua Moody <joshuajmoody@gmail.com>
2015-07-04 09:44:38 +0200

    Update LPDDRange with newest sources

--- calabash/Classes/CocoaHttpServer/Categories/LPDDRange.m
+++ calabash/Classes/CocoaHttpServer/Categories/LPDDRange.m
@@ -1,104 +1,91 @@
 #import "LPDDRange.h"
 #import "NSNumber+LPCHSExtensions.h"
 
-LPDDRange LPDDUnionRange(LPDDRange range1, LPDDRange range2)
-{
-	LPDDRange result;
-	
-	result.location = MIN(range1.location, range2.location);
-	result.length   = MAX(LPDDMaxRange(range1), LPDDMaxRange(range2)) - result.location;
-	
-	return result;
+LPDDRange LPDDUnionRange(LPDDRange range1, LPDDRange range2) {
+  LPDDRange result;
+
+  result.location = MIN(range1.location, range2.location);
+  result.length = MAX(LPDDMaxRange(range1), LPDDMaxRange(range2)) - result.location;
+
+  return result;
 }
 
-LPDDRange LPDDIntersectionRange(LPDDRange range1, LPDDRange range2)
-{
-	LPDDRange result;
-	
-	if((LPDDMaxRange(range1) < range2.location) || (LPDDMaxRange(range2) < range1.location))
-	{
-		return LPDDMakeRange(0, 0);
-	}
-	
-	result.location = MAX(range1.location, range2.location);
-	result.length   = MIN(LPDDMaxRange(range1), LPDDMaxRange(range2)) - result.location;
-	
-	return result;
+LPDDRange LPDDIntersectionRange(LPDDRange range1, LPDDRange range2) {
+  LPDDRange result;
+
+  if ((LPDDMaxRange(range1) < range2.location) || (LPDDMaxRange(range2) < range1.location)) {
+    return LPDDMakeRange(0, 0);
+  }
+
+  result.location = MAX(range1.location, range2.location);
+  result.length = MIN(LPDDMaxRange(range1), LPDDMaxRange(range2)) - result.location;
+
+  return result;
 }
 
-NSString *LPDDStringFromRange(LPDDRange range)
-{
-	return [NSString stringWithFormat:@"{%qu, %qu}", range.location, range.length];
+NSString *LPDDStringFromRange(LPDDRange range) {
+  return [NSString stringWithFormat:@"{%qu, %qu}", range.location, range.length];
 }
 
-LPDDRange LPDDRangeFromString(NSString *aString)
-{
-	LPDDRange result = LPDDMakeRange(0, 0);
-	
-	// NSRange will ignore '-' characters, but not '+' characters
-	NSCharacterSet *cset = [NSCharacterSet characterSetWithCharactersInString:@"+0123456789"];
-	
-	NSScanner *scanner = [NSScanner scannerWithString:aString];
-	[scanner setCharactersToBeSkipped:[cset invertedSet]];
-	
-	NSString *str1 = nil;
-	NSString *str2 = nil;
-	
-	BOOL found1 = [scanner scanCharactersFromSet:cset intoString:&str1];
-	BOOL found2 = [scanner scanCharactersFromSet:cset intoString:&str2];
-	
-	if(found1) [NSNumber parseString:str1 intoUInt64:&result.location];
-	if(found2) [NSNumber parseString:str2 intoUInt64:&result.length];
-	
-	return result;
+LPDDRange LPDDRangeFromString(NSString *aString) {
+  LPDDRange result = LPDDMakeRange(0, 0);
+
+  // NSRange will ignore '-' characters, but not '+' characters
+  NSCharacterSet *cset = [NSCharacterSet characterSetWithCharactersInString:@"+0123456789"];
+
+  NSScanner *scanner = [NSScanner scannerWithString:aString];
+  [scanner setCharactersToBeSkipped:[cset invertedSet]];
+
+  NSString *str1 = nil;
+  NSString *str2 = nil;
+
+  BOOL found1 = [scanner scanCharactersFromSet:cset intoString:&str1];
+  BOOL found2 = [scanner scanCharactersFromSet:cset intoString:&str2];
+
+  if (found1) [NSNumber parseString:str1 intoUInt64:&result.location];
+  if (found2) [NSNumber parseString:str2 intoUInt64:&result.length];
+
+  return result;
 }
 
-NSInteger LPDDRangeCompare(LPDDRangePointer pDDRange1, LPDDRangePointer pDDRange2)
-{
-	// Comparison basis:
-	// Which range would you encouter first if you started at zero, and began walking towards infinity.
-	// If you encouter both ranges at the same time, which range would end first.
-	
-	if(pDDRange1->location < pDDRange2->location)
-	{
-		return NSOrderedAscending;
-	}
-	if(pDDRange1->location > pDDRange2->location)
-	{
-		return NSOrderedDescending;
-	}
-	if(pDDRange1->length < pDDRange2->length)
-	{
-		return NSOrderedAscending;
-	}
-	if(pDDRange1->length > pDDRange2->length)
-	{
-		return NSOrderedDescending;
-	}
-	
-	return NSOrderedSame;
+NSInteger LPDDRangeCompare(LPDDRangePointer pLPDDRange1, LPDDRangePointer pLPDDRange2) {
+  // Comparison basis:
+  // Which range would you encouter first if you started at zero, and began walking towards infinity.
+  // If you encouter both ranges at the same time, which range would end first.
+
+  if (pLPDDRange1->location < pLPDDRange2->location) {
+    return NSOrderedAscending;
+  }
+  if (pLPDDRange1->location > pLPDDRange2->location) {
+    return NSOrderedDescending;
+  }
+  if (pLPDDRange1->length < pLPDDRange2->length) {
+    return NSOrderedAscending;
+  }
+  if (pLPDDRange1->length > pLPDDRange2->length) {
+    return NSOrderedDescending;
+  }
+
+  return NSOrderedSame;
 }
 
-@implementation NSValue (LPNSValueDDRangeExtensions)
+@implementation NSValue (NSValueLPDDRangeExtensions)
 
-+ (NSValue *)valueWithDDRange:(LPDDRange)range
-{
-	return [NSValue valueWithBytes:&range objCType:@encode(LPDDRange)];
++ (NSValue *)valueWithLPDDRange:(LPDDRange)range {
+  return [NSValue valueWithBytes:&range objCType:@encode(LPDDRange)];
 }
 
-- (LPDDRange)ddrangeValue
-{
-	LPDDRange result;
-	[self getValue:&result];
-	return result;
+- (LPDDRange)lpDDRangeValue {
+  LPDDRange result;
+  [self getValue:&result];
+  return result;
 }
 
-- (NSInteger)ddrangeCompare:(NSValue *)other
-{
-	LPDDRange r1 = [self ddrangeValue];
-	LPDDRange r2 = [other ddrangeValue];
-	
-	return LPDDRangeCompare(&r1, &r2);
+- (NSInteger)lpDDRangeCompare:(NSValue *)other {
+  LPDDRange r1 = [self lpDDRangeValue];
+  LPDDRange r2 = [other lpDDRangeValue];
+
+  return LPDDRangeCompare(&r1, &r2);
 }
 
 @end
