a719ac40fc6e745baecb03dbd70185f396efbed9
Joshua Moody <joshuajmoody@gmail.com>
2015-07-05 14:20:32 +0200

    LPBackdoor fixup the unknown selector reporting

--- calabash/Classes/FranklyServer/Routes/LPBackdoorRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPBackdoorRoute.m
@@ -57,36 +57,29 @@
     return  @{ @"result" : result, @"outcome" : @"SUCCESS" };
   } else {
 
-    NSString *details = [NSString stringWithFormat:@"you must define the selector '%@' in your UIApplicationDelegate.",
-                                                   selectorName];
-    NSString *exDecl0 = @"// declaration";
-    NSString *exDecl1 = [NSString stringWithFormat:@"-(id)%@ (id)arg;", selectorName];
-    NSString *exImp0 = @"// implementation";
-    NSString *exImp1 = [NSString stringWithFormat:@"-(id)%@ (id)anArg {",
-                                                  selectorName];
-    NSString *exImp2 = [NSString stringWithFormat:@"  // do custom stuff"];
-    NSString *exImp3 = [NSString stringWithFormat:@"  // return examples"];
-    NSString *exImp4 = [NSString stringWithFormat:@"  return @\"OK\";"];
-    NSString *exImp5 = [NSString stringWithFormat:@"  return [NSArray arrayWithObjects:@\"a\",@\"b\",nil];"];
-    NSString *exImp6 = [NSString stringWithFormat:@"  return nil;"];
-    NSString *exImp7 = [NSString stringWithFormat:@"}"];
-    NSString *usage0 = [NSString stringWithFormat:@"// Ruby usage"];
-    NSString *usage1 = [NSString stringWithFormat:@"backdoor('%@', '<arg>')",
-                                                  selectorName];
+    NSArray *lines =
+    @[
+      @"",
+      [NSString stringWithFormat:@"You must define '%@' in your UIApplicationDelegate.",
+       selectorName],
+      @"",
+      [NSString stringWithFormat:@"// Example"],
+      [NSString stringWithFormat:@"-(NSString *)%@(NSString *)argument {", selectorName],
+      [NSString stringWithFormat:@"  // do stuff here"],
+      [NSString stringWithFormat:@"  return @\"a result\";"],
+      [NSString stringWithFormat:@"}"],
+      @"",
+      @"// Documentation",
+      @"http://developer.xamarin.com/guides/testcloud/calabash/working-with/backdoors/#backdoor_in_iOS",
+      @"",
+      @""
+      ];
 
-    NSArray *exArr = [NSArray arrayWithObjects:details, @"\n", exDecl0, exDecl1,
-                                               @"\n", exImp0, exImp1, exImp2,
-                                               exImp3, exImp4, exImp5, exImp6,
-                                               exImp7, @"\n", usage0, usage1,
-                                               nil];
-    NSString *detailsStr = [exArr componentsJoinedByString:@"\n"];
+    NSString *details = [lines componentsJoinedByString:@"\n"];
 
-    NSString *reasonStr = [NSString stringWithFormat:@"application delegate does not respond to selector '%@'",
-                                                     selectorName];
-    return [NSDictionary dictionaryWithObjectsAndKeys:detailsStr, @"details",
-                                                      reasonStr, @"reason",
-                                                      @"FAILURE", @"outcome",
-                                                      nil];
+    NSString *reason = [NSString stringWithFormat:@"The backdoor: '%@' is undefined.",
+                        selectorName];
+    return  @{ @"details" : details, @"reason" : reason, @"outcome" : @"FAILURE" };
   }
 }
 
