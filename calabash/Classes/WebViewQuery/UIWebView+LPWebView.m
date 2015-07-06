4586fe1c5457e1e878387a647348db00200a12d4
Joshua Moody <joshuajmoody@gmail.com>
2015-07-06 18:55:30 +0200

    Ensure UIWebView eval js is run on the main thread

--- calabash/Classes/WebViewQuery/UIWebView+LPWebView.m
+++ calabash/Classes/WebViewQuery/UIWebView+LPWebView.m
@@ -7,7 +7,15 @@
 @implementation UIWebView (UIWebView_LPWebView)
 
 - (NSString *) calabashStringByEvaluatingJavaScript:(NSString *) javascript {
-  return [self stringByEvaluatingJavaScriptFromString:javascript];
+  if ([[NSThread currentThread] isMainThread]) {
+    return [self stringByEvaluatingJavaScriptFromString:javascript];
+  } else {
+    __block NSString *result = nil;
+    dispatch_sync(dispatch_get_main_queue(), ^{
+      result = [self stringByEvaluatingJavaScriptFromString:javascript];
+    });
+    return result;
+  }
 }
 
 @end
