212a48e9236052e77471dab9ca64e7d9c00db722
Joshua Moody <joshuajmoody@gmail.com>
2015-07-07 08:29:52 +0200

    Remove the useless code comment about copyright

--- VersionTool/main.m
+++ VersionTool/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  version
-//
-//  Created by Joshua Moody on 27.3.14.
-//  Copyright (c) 2014 Xamarin. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import "LPVersionRoute.h"
 
