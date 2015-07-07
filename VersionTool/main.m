4d610bbdfd48ee2eb55ee397f4a721fae9d12f98
Joshua Moody <joshuajmoody@gmail.com>
2015-07-07 08:30:14 +0200

    Import and define git version constants

--- VersionTool/main.m
+++ VersionTool/main.m
@@ -1,5 +1,24 @@
 #import <Foundation/Foundation.h>
 #import "LPVersionRoute.h"
+#import "LPGitVersionDefines.h"
+
+#ifdef LP_GIT_SHORT_REVISION
+static NSString *const kLPGitShortRevision = LP_GIT_SHORT_REVISION;
+#else
+static NSString *const kLPGitShortRevision = @"Unknown";
+#endif
+
+#ifdef LP_GIT_BRANCH
+static NSString *const kLPGitBranch = LP_GIT_BRANCH;
+#else
+static NSString *const kLPGitBranch = @"Unknown";
+#endif
+
+#ifdef LP_GIT_REMOTE_ORIGIN
+static NSString *const kLPGitRemoteOrigin = LP_GIT_REMOTE_ORIGIN;
+#else
+static NSString *const kLPGitRemoteOrigin = @"Unknown";
+#endif
 
 int main(int argc, const char * argv[]) {
 
