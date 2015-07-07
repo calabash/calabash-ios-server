37c6292f0171b0ac5e796d648410887b78419d9d
Joshua Moody <joshuajmoody@gmail.com>
2015-07-07 09:27:33 +0200

    Remove bogus copyright comment

--- calabash/Classes/FranklyServer/Operations/LPScrollToRowWithMarkOperation.m
+++ calabash/Classes/FranklyServer/Operations/LPScrollToRowWithMarkOperation.m
@@ -1,10 +1,3 @@
-//
-//  ScrollOperation.m
-//  Calabash
-//
-//  Created by Karl Krukow on 18/08/11.
-//  Copyright (c) 2011 LessPainful. All rights reserved.
-//
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
