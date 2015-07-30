92c1d85ec8d47b1c25b5513d11c2426f46eb4aed
Joshua Moody <joshuajmoody@gmail.com>
2015-07-30 12:28:57 +0200

    WIP add XCTest for keyboard language route

---
+++ XCTest/Tests/Routes/LPKeyboardLanguageRouteTest.m
@@ -0,0 +1,36 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LPKeyboardLanguageRouteTest : XCTestCase
+
+@end
+
+@implementation LPKeyboardLanguageRouteTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
