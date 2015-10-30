e5216c653afa399aa5ecf6ffc5556d7cf027e8c8
Joshua Moody <joshuajmoody@gmail.com>
2015-10-30 14:35:17 +0100

    Cucumber: ran calabash-ios gen

---
+++ cucumber/features/support/env.rb
@@ -0,0 +1,9 @@
+# Requiring this file will import Calabash and the Calabash predefined Steps.
+require 'calabash-cucumber/cucumber'
+
+# To use Calabash without the predefined Calabash Steps, uncomment these
+# three lines and delete the require above.
+# require 'calabash-cucumber/wait_helpers'
+# require 'calabash-cucumber/operations'
+# World(Calabash::Cucumber::Operations)
+
