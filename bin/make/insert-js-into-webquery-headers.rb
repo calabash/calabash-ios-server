3e38c3ca0304af3ddf1ca3174d898b1bf1afb139
Joshua Moody <joshuajmoody@gmail.com>
2016-01-05 11:27:07 +0100

    Make: add logging and condition checking to write_header

--- bin/make/insert-js-into-webquery-headers.rb
+++ bin/make/insert-js-into-webquery-headers.rb
@@ -6,11 +6,16 @@ require "luffa"
 module Calabash
 
   def self.write_header(header_file, constant_name, minified_js_file)
+    self.log_banner("#{File.basename(header_file)}")
+
     header_lines = []
+
+    match_found = false
 
     IO.read(header_file).force_encoding("utf-8").each_line do |line|
       if /#{constant_name}/.match(line)
-        puts "Found match for #{constant_name}"
+        Calabash.log_debug("Found match for #{constant_name}.")
+        match_found = true
         line.strip!
         new_js = IO.read(minified_js_file).force_encoding("utf-8").strip
         header_lines << %Q[static NSString *#{constant_name} = @"#{new_js}";]
@@ -19,9 +24,66 @@ module Calabash
       end
     end
 
+    if !match_found
+      ["Could not find a match for #{constant_name} in",
+       "",
+       " #{header_file}",
+       "",
+       "Inserting #{File.basename(minified_js_file)} failed."].each do |line|
+         Calabash.log_error(line)
+       end
+      exit 1
+    end
+
     File.open(header_file, "w:UTF-8") do |f|
       f.puts(header_lines)
     end
+
+    self.log_info("Injected #{File.basename(minified_js_file)} into #{constant_name}")
+  end
+
+  def self.debug?
+    ENV["DEBUG"] == "1"
+  end
+
+  def self.log_debug(string)
+    puts self.magenta("DEBUG: #{string}") if self.debug?
+  end
+
+  def self.log_info(string)
+    puts self.green("INFO:  #{string}")
+  end
+
+  def self.log_error(string)
+    puts self.red("ERROR: #{string}")
+  end
+
+  def self.log_banner(string)
+    puts self.magenta("\n### #{string} ###\n")
+  end
+
+  def self.log_unix_command(string)
+    puts self.cyan("EXEC: #{string}")
+  end
+
+  def self.colorize(string, color)
+		"\033[#{color}m#{string}\033[0m"
+  end
+
+  def self.red(string)
+    colorize(string, 31)
+  end
+
+  def self.magenta(string)
+    colorize(string, 35)
+  end
+
+  def self.green(string)
+    colorize(string, 32)
+  end
+
+  def self.cyan(string)
+    colorize(string, 36)
   end
 end
 
