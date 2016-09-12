33c00a05304be405dfbeb98c11c02db901ea9551
Joshua Moody <joshuajmoody@gmail.com>
2016-09-12 10:20:09 +0200

    Server: bump to version to 0.20.0

--- calabash/Classes/FranklyServer/Routes/LPVersionRoute.h
+++ calabash/Classes/FranklyServer/Routes/LPVersionRoute.h
@@ -21,7 +21,7 @@
  Do not change the 'CALABASH VERSION' portion of the following constant without
  updating the ruby API.
  ******************/
-#define kLPCALABASHVERSION @"CALABASH VERSION: 0.19.2"
+#define kLPCALABASHVERSION @"CALABASH VERSION: 0.20.0"
 
 @interface LPVersionRoute : NSObject <LPRoute>
 
