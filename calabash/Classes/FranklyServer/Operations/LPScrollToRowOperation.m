5d58246ebcd254a7130727215de4fa232a036abd
Joshua Moody <joshuajmoody@gmail.com>
2016-09-22 16:48:32 +0200

    Server: update scroll to row operation

--- calabash/Classes/FranklyServer/Operations/LPScrollToRowOperation.m
+++ calabash/Classes/FranklyServer/Operations/LPScrollToRowOperation.m
@@ -29,59 +29,64 @@
   return nil;
 }
 
-
 - (id) performWithTarget:(id) target error:(NSError *__autoreleasing*) error {
   NSArray *arguments = self.arguments;
 
-  if ([target isKindOfClass:[UITableView class]]) {
-    UITableView *table = (UITableView *) target;
-    NSNumber *rowNum = [arguments objectAtIndex:0];
-    if ([arguments count] >= 2) {
-      NSInteger row = [rowNum integerValue];
-      NSInteger sec = [[arguments objectAtIndex:1] integerValue];
-      NSIndexPath *path = [NSIndexPath indexPathForRow:row inSection:sec];
-
-      if ((sec >= 0 && sec < [table numberOfSections]) && (row >= 0 && row < [table numberOfRowsInSection:sec])) {
-        UITableViewScrollPosition sp = UITableViewScrollPositionTop;
-        BOOL animate = YES;
-        if ([arguments count] >= 3) {
-          NSString *pos = [arguments objectAtIndex:2];
-          if ([@"middle" isEqualToString:pos]) {
-            sp = UITableViewScrollPositionMiddle;
-          } else if ([@"bottom" isEqualToString:pos]) {
-            sp = UITableViewScrollPositionBottom;
-          } else if ([@"none" isEqualToString:pos]) {
-            sp = UITableViewScrollPositionNone;
-          }
-        }
-        if ([arguments count] >= 4) {
-          NSNumber *ani = [arguments objectAtIndex:3];
-          animate = [ani boolValue];
-        }
-
-        [table scrollToRowAtIndexPath:path
-                     atScrollPosition:sp
-                             animated:animate];
-
-        return target;
-      } else {
-        LPLogWarn(@"Table doesn't contain indexPath: %@", path);
-        return nil;
-      }
-    } else {
-      NSIndexPath *indexPathForRow = [self indexPathForRow:[rowNum unsignedIntegerValue]
-                                                   inTable:table];
-      if (!indexPathForRow) { return nil;  }
-
-      [table scrollToRowAtIndexPath:indexPathForRow
-                   atScrollPosition:UITableViewScrollPositionTop
-                           animated:YES];
-      return target;
-    }
+  if (!target) {
+    LPLogWarn(@"Cannot perform operation on nil target");
+    return nil;
   }
 
- LPLogWarn(@"View %@ should be a table view for scrolling to row/cell to make sense",
-          target);
-  return nil;
+  if (![[target class] isSubclassOfClass:[UITableView class]]) {
+    LPLogWarn(@"View: %@ should be an instance of UITableView but found '%@'",
+              target, NSStringFromClass([target class]));
+    return nil;
+  }
+
+  UITableView *table = (UITableView *) target;
+  NSNumber *rowNum = [arguments objectAtIndex:0];
+  if ([arguments count] >= 2) {
+    NSInteger row = [rowNum integerValue];
+    NSInteger sec = [[arguments objectAtIndex:1] integerValue];
+    NSIndexPath *path = [NSIndexPath indexPathForRow:row inSection:sec];
+
+    if ((sec >= 0 && sec < [table numberOfSections]) && (row >= 0 && row < [table numberOfRowsInSection:sec])) {
+      UITableViewScrollPosition sp = UITableViewScrollPositionTop;
+      BOOL animate = YES;
+      if ([arguments count] >= 3) {
+        NSString *pos = [arguments objectAtIndex:2];
+        if ([@"middle" isEqualToString:pos]) {
+          sp = UITableViewScrollPositionMiddle;
+        } else if ([@"bottom" isEqualToString:pos]) {
+          sp = UITableViewScrollPositionBottom;
+        } else if ([@"none" isEqualToString:pos]) {
+          sp = UITableViewScrollPositionNone;
+        }
+      }
+      if ([arguments count] >= 4) {
+        NSNumber *ani = [arguments objectAtIndex:3];
+        animate = [ani boolValue];
+      }
+
+      [table scrollToRowAtIndexPath:path
+                   atScrollPosition:sp
+                           animated:animate];
+
+      return target;
+    } else {
+      LPLogWarn(@"Table doesn't contain indexPath: %@", path);
+      return nil;
+    }
+  } else {
+    NSIndexPath *indexPathForRow = [self indexPathForRow:[rowNum unsignedIntegerValue]
+                                                 inTable:table];
+    if (!indexPathForRow) { return nil;  }
+
+    [table scrollToRowAtIndexPath:indexPathForRow
+                 atScrollPosition:UITableViewScrollPositionTop
+                         animated:YES];
+    return target;
+  }
 }
+
 @end
