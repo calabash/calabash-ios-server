d71277ace74484f5c26ca6d54430f8a556b5e5eb
Joshua Moody <joshuajmoody@gmail.com>
2018-05-04 12:39:06 +0200

    Bin: versioning-after script passes if file is missing

--- bin/xcode-build-phase/gitversioning-after.sh
+++ bin/xcode-build-phase/gitversioning-after.sh
@@ -4,9 +4,9 @@ source bin/log.sh
 
 DEFINES_HEADER="calabash/LPGitVersionDefines.h"
 if [ ! -e "${DEFINES_HEADER}" ]; then
-  error "${DEFINES_HEADER} file needs to exist"
-  exit 1
+  info "Defines header does not exist: ${DEFINES_HEADER}"
+  info "Nothing to do"
+else
+  info "Reseting the contents of ${DEFINES_HEADER}"
+  git checkout -- "${DEFINES_HEADER}"
 fi
-
-info "Reseting the contents of ${DEFINES_HEADER}"
-git checkout -- "${DEFINES_HEADER}"
