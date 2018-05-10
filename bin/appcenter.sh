52b092e5ac52ccd30376d3e68e3867ff2951c0c4
Tikhon Polyakov <polyakrecords@gmail.com>
2018-05-10 20:33:16 +0700

    Bin: update scripts to submit tests in AppCenter instead of XTC

--- bin/appcenter.sh
+++ bin/appcenter.sh
@@ -9,9 +9,10 @@ if [ -z ${1} ]; then
 
 Examples:
 
-$ bin/xtc.sh e9232255
-$ SKIP_IPA_BUILD=1 SERIES='Args and env' bin/xtc.sh e9232255
-$ SERIES='DeviceAgent 2.0' bin/xtc.sh e9232255 48d137d6228ccda303b2a71b0d09e1d0629bf980
+$ bin/appcenter.sh e9232255
+$ bin/appcenter.sh organization/device-set
+$ SKIP_IPA_BUILD=1 SERIES='Args and env' bin/appcenter.sh e9232255
+$ SERIES='DeviceAgent 2.0' bin/appcenter.sh e9232255 48d137d6228ccda303b2a71b0d09e1d0629bf980
 
 The DeviceAgent-SHA optional argument allows tests to be run against any
 DeviceAgent that has been uploaded to S3 rather than the current active
@@ -24,24 +25,21 @@ Responds to these env variables:
 
         SERIES: the Test Cloud series
 SKIP_IPA_BUILD: iff 1, then skip re-building the ipa.
-                'make test-ipa' will still be called, so changes in the
+                'make ipa-cal' will still be called, so changes in the
                 features/ directory will be staged and sent to Test Cloud.
 BUILD_RUN_LOOP: iff 1, then rebuild run-loop gem before uploading.
-BUILD_RUN_LOOP: iff 1, then rebuild Calabash iOS gem before uploading.
 
 "
 
   exit 64
 fi
 
-CREDS=.xtc-credentials
+CREDS=.appcenter-credentials
 if [ ! -e "${CREDS}" ]; then
   error "This script requires a ${CREDS} file"
   error "Generating a template now:"
   cat >${CREDS} <<EOF
-export XTC_PRODUCTION_API_TOKEN=
-export XTC_STAGING_API_TOKEN=
-export XTC_USER=
+export APPCENTER_TOKEN=
 EOF
   cat ${CREDS}
   error "Update the file with your credentials and run again."
@@ -58,62 +56,35 @@ if [ "${BUILD_RUN_LOOP}" = "1" ]; then
   (cd ../run_loop; rake install)
 fi
 
-if [ "${BUILD_CALABASH}" = "1" ]; then
-  gem uninstall -Vax --force --no-abort-on-dependent calabash-cucumber
-  (cd ../calabash-ios/calabash-cucumber; rake install)
-fi
+PREPARE_TC_ONLY="${SKIP_IPA_BUILD}" make ipa-cal
 
-PREPARE_XTC_ONLY="${SKIP_IPA_BUILD}" make ipa-cal
-
-cd xtc-submit
+(cd testcloud-submit
 
 rm -rf .xtc
 mkdir -p .xtc
 
 if [ "${2}" != "" ]; then
   echo "${2}" > .xtc/device-agent-sha
-fi
-
-if [ "${SERIES}" = "" ]; then
-  SERIES=master
-fi
-
-if [ -z $XTC_ENDPOINT ]; then
-  API_TOKEN="${XTC_PRODUCTION_API_TOKEN}"
-  ENDPOINT="${XTC_PRODUCTION_ENDPOINT}"
-  info "Uploading to Production"
-else
-  API_TOKEN="${XTC_STAGING_API_TOKEN}"
-  ENDPOINT="${XTC_STAGING_ENDPOINT}"
-  info "Uploading to Staging"
-fi
-
-# To submit to a pipeline branch, replace <branch> and append to
-# PARAMETERS
-PIPELINE="<branch>"
+fi)
 
 S3_ROOT="https://s3-eu-west-1.amazonaws.com/calabash-files/dylibs/to-test-injection"
 LIB_BEETS="${S3_ROOT}/libBetaVulgaris.dylib"
 LIB_CABBAGE="${S3_ROOT}/libBrassica.dylib"
 LIB_CUCUMBER="${S3_ROOT}/libCucurbits.dylib"
-INJECT="inject:${LIB_BEETS};${LIB_CABBAGE};${LIB_CUCUMBER}"
+INJECT="inject=${LIB_BEETS};${LIB_CABBAGE};${LIB_CUCUMBER}"
+APP_ENV="app_env=ARG_FROM_UPLOADER_FOR_AUT=From-the-CLI-uploader!"
 
-APP_ENV="app_env:ARG_FROM_UPLOADER_FOR_AUT=From-the-CLI-uploader!"
-
-# Required for Test Cloud Scenarios to pass.
-# Parameters always needs to include these!
-PARAMETERS="${APP_ENV} ${INJECT}"
-
-XTC_ENDPOINT="${ENDPOINT}" \
-bundle exec test-cloud submit \
-  TestApp.ipa \
-  $API_TOKEN \
-  --user $XTC_USER \
-  --app-name "TestApp" \
+appcenter test run calabash \
+  --debug \
+  --app-path testcloud-submit/LPTestTarget.ipa \
+  --app App-Center-Test-Cloud/LPTestTarget-iOS \
+  --project-dir testcloud-submit \
+  --token $APPCENTER_TOKEN \
   --devices "${1}" \
-  --series "${SERIES}" \
-  --config cucumber.yml \
+  --config-path cucumber.yml \
   --profile default \
-  --dsym-file "TestApp.app.dSYM" \
   --include .xtc \
-  --test-parameters ${PARAMETERS}
+  --test-parameter ${INJECT} \
+  --test-parameter ${APP_ENV} \
+  --test-series ${SERIES} \
+  --disable-telemetry
