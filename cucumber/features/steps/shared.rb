4d2a6aea53e4a208e9a803beb97f7db96eaea1cc
Maxim Lobanov <v-malob@microsoft.com>
2019-08-06 09:14:26 +0300

    Fix logic to switch to the second tab (#444)
    * enable test + add while
    
    * move code to the function

--- cucumber/features/steps/shared.rb
+++ cucumber/features/steps/shared.rb
@@ -8,6 +8,13 @@ module LPTestTarget
       body = http({:method => "GET", :path => path}, {})
       response_body_to_hash(body)
     end
+
+    def switch_to_second_tab_retryable(attempt_count)
+      begin
+        touch("UITabBarButton index:1")
+        sleep(0.5)
+      end while attempt_count > 0 && query("view marked: 'Second View'").empty?
+    end
   end
 end
 
@@ -37,7 +44,7 @@ And(/^I go to the second tab$/) do
     !query("UITabBarButton").empty?
   end
 
-  touch("UITabBarButton index:1")
+  switch_to_second_tab_retryable(5)
   wait_for_none_animating
 end
 
