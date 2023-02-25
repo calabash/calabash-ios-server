fbcc913c703ec0349c7dc4192692b6b50f18dac6
Ilya Bausov <ilya.bausov@akvelon.com>
2023-02-25 15:27:29 +0100

    Remove CocoaLumberjack sources from repository.

--- calabash/Classes/CocoaLumberjack/LPASLLogFormatter.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "LPCocoaLumberjack.h"
-
-@interface LPASLLogFormatter : NSObject <LPLogFormatter>
-
-@end
--- calabash/Classes/CocoaLumberjack/LPTTYLogFormatter.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "LPCocoaLumberjack.h"
-
-@interface LPTTYLogFormatter : NSObject <LPLogFormatter>
-
-@end
