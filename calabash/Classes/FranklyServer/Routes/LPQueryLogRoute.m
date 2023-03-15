04ce0c2b40538627a4df81a0d003a18cd8e1e859
Ilya Bausov <ilya.bausov@akvelon.com>
2023-03-15 09:32:22 +0100

    Add stub to the LPQueryLogRoute JSONResponseForMethod.

--- calabash/Classes/FranklyServer/Routes/LPQueryLogRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPQueryLogRoute.m
@@ -10,8 +10,6 @@
 //
 
 #import "LPQueryLogRoute.h"
-#import "asl.h"
-#import <os/log.h>
 
 @implementation LPQueryLogRoute
 
@@ -19,94 +17,10 @@
   return [method isEqualToString:@"POST"];
 }
 
-- (NSDictionary *) JSONResponseForMethod:(NSString *) method
-                                     URI:(NSString *) path
-                                    data:(NSDictionary *) data {
-  int count = 0;
-
-  //Build a query message containing all our criteria.
-  os_log(OS_LOG_DEFAULT, "");
-  aslmsg query = asl_new(ASL_TYPE_QUERY);
-
-  for (__strong NSString *key in [data keyEnumerator]) {
-    int op = ASL_QUERY_OP_EQUAL;
-    NSString *kData = [data valueForKey:key];
-
-    if ([key isEqualToString:@"count"]) {
-      count = [kData intValue];
-      continue;
-    }
-
-    //If the key string contains one or more dots, process those modifiers.
-    //Put the operation first, the modifiers following.
-    if ([key rangeOfString:@"."].location != NSNotFound) {
-      NSArray *keyParts = [key componentsSeparatedByString:@"."];
-      key = [keyParts objectAtIndex:0];
-      for (int i = 1; i < [keyParts count]; i++) {
-        NSString *modifier = [[keyParts objectAtIndex:i] lowercaseString];
-        if ([modifier isEqualToString:@"equal"]) {
-          op = ASL_QUERY_OP_EQUAL;
-        } else if ([modifier isEqualToString:@"greater"]) {
-          op = ASL_QUERY_OP_GREATER;
-        } else if ([modifier isEqualToString:@"greater_equal"]) {
-          op = ASL_QUERY_OP_GREATER_EQUAL;
-        } else if ([modifier isEqualToString:@"less"]) {
-          op = ASL_QUERY_OP_LESS;
-        } else if ([modifier isEqualToString:@"less_equal"]) {
-          op = ASL_QUERY_OP_LESS_EQUAL;
-        } else if ([modifier isEqualToString:@"not_equal"]) {
-          op = ASL_QUERY_OP_NOT_EQUAL;
-        } else if ([modifier isEqualToString:@"regex"]) {
-          op = ASL_QUERY_OP_REGEX;
-        } else if ([modifier isEqualToString:@"true"]) {
-          op = ASL_QUERY_OP_TRUE;
-        } else if ([modifier isEqualToString:@"casefold"]) {
-          op |= ASL_QUERY_OP_CASEFOLD;
-        } else if ([modifier isEqualToString:@"prefix"]) {
-          op |= ASL_QUERY_OP_PREFIX;
-        } else if ([modifier isEqualToString:@"suffix"]) {
-          op |= ASL_QUERY_OP_SUFFIX;
-        } else if ([modifier isEqualToString:@"substring"]) {
-          op |= ASL_QUERY_OP_SUBSTRING;
-        } else if ([modifier isEqualToString:@"numeric"]) {
-          op |= ASL_QUERY_OP_NUMERIC;
-        }
-        //ignore unknown values
-      }
-    }
-
-    //If we get here, assume the key is a ASL field to be set.
-    //Note this means the user must pass in correct key strings such as "Facility" or "Sender".
-    asl_set_query(query, [key UTF8String], [kData UTF8String], op);
-  }
-
-  //Begin the search.
-  aslresponse response = asl_search(NULL, query);
-
-  asl_free(query);
-
-  NSMutableArray *messages = [[NSMutableArray alloc] init];
-  aslmsg msg;
-
-  while ((msg = asl_next(response)) && count-- > 0) {
-    //Load all the key/value pairs from the message into a dictionary.
-    const char *k;
-    NSMutableDictionary *msgDict = [[NSMutableDictionary alloc] init];
-    for (unsigned i = 0U; (k = asl_key(msg, i)); ++i) {
-      const char *v = asl_get(msg, k);
-      [msgDict setObject:[NSString stringWithUTF8String:v]
-                  forKey:[NSString stringWithUTF8String:k]];
-    }
-    [messages addObject:msgDict];
-  }
-
-  asl_release(response);
-
-  NSArray *results = [NSArray arrayWithArray:messages];
-
+- (NSDictionary *) JSONResponseForMethod:(NSString *) method URI:(NSString *) path data:(NSDictionary *) data {
   return
   @{
-    @"results" : @"",
+    @"results" : @"Querying logs are deprecated now.",
     @"outcome" : @"SUCCESS",
     };
 }
